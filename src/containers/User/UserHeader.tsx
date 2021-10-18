0aca33c5b6a2b4e07fd39f5f06b357eb4320449f
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-10-18 03:51:42 +0200

    ✨ button edit profile on owner's profile

--- src/containers/User/UserHeader.tsx
+++ src/containers/User/UserHeader.tsx
@@ -6,6 +6,10 @@ import { User } from "../../types/entities/User"
 import { Avatar } from "../../components/User/Avatar"
 import { getUserName } from "../../utils/user"
 import nl2br from "react-nl2br"
+import { useContext } from "react"
+import { UserContext } from "../UserProvider"
+import Link from "next/link"
+import { Button } from "../../components/Button"
 
 
 interface Props {
@@ -13,12 +17,32 @@ interface Props {
 }
 
 export function UserHeader({ user }: Props) {
+  const userCtx = useContext(UserContext)
+  const userConnected = userCtx.user!
+
   return (
     <header className={cs(style.container, layout['padding-small'])}>
       <Avatar uri={user.avatarUri} className={cs(style.avatar, effects['drop-shadow-big'])} />
       <div>
         <h1>{ getUserName(user) }</h1>
         <p>{ nl2br(user.description) }</p>
+        {userConnected && userConnected.id === user.id && (
+          <div style={{
+            display: "inline-block"
+          }}>
+            <Link href="/edit-profile" passHref>
+              <Button
+                isLink
+                style={{
+                  alignSelf: "flex-start"
+                }}
+                size="small"
+              >
+                edit profile
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   )
