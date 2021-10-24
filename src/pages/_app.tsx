0ce0f3c8a3b79fc8689f31296b0fcf344d60caf8
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-10-24 04:28:20 +0200

    ✨ added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta key="og:description" property="og:description" content="fxhash is a platform to mint Generative Tokens on the Tezos blockchain"/>
         <meta key="og:type" property="og:type" content="website"/>
         <meta key="og:image" property="og:image" content="/images/og/og1.jpg"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
 
       <ApolloProvider client={clientSideClient}>
