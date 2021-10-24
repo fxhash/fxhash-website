f967009ca9c24a48cd42c3f3a009bab10b60324c
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-10-24 04:09:42 +0200

    ⚡️ lazy loading of the card thumbnails

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,10 @@
 import style from "./Card.module.scss"
 import cs from "classnames"
-import { PropsWithChildren, useMemo } from "react"
+import { PropsWithChildren, useMemo, useState } from "react"
 import { ipfsDisplayUrl } from "../../services/Ipfs"
+import { useClientAsyncEffect } from "../../utils/hookts"
+import { Loader } from "../Utils/Loader"
+import { useInView } from "react-intersection-observer"
 
 
 interface Props {
@@ -12,16 +15,36 @@ export function Card({
   thumbnailUri,
   children
 }: PropsWithChildren<Props>) {
+  const [loaded, setLoaded] = useState<string|null>(null)
   const url = useMemo(() => ipfsDisplayUrl(thumbnailUri), [])
+  const { ref, inView } = useInView()
+
+  // lazy load the image
+  useClientAsyncEffect(isMounted => {
+    if (inView && !loaded && url) {
+      const img = new Image()
+      img.onload = () => {
+        if (isMounted()) {
+          setLoaded(url)
+        }
+      }
+      img.src = url
+    }
+  }, [inView])
 
   return (
-    <div className={cs(style.container)}>
+    <div className={cs(style.container)} ref={ref}>
       <div 
         className={cs(style['thumbnail-container'])}
         style={{
-          backgroundImage: `url(${url})`
+          backgroundImage: loaded ? `url(${loaded})` : "none"
         }}
       >
+        {!loaded && (
+          <div className={cs(style.loader)}>
+            <Loader color="white" />
+          </div>
+        )}
         {!url && (
           <div className={cs(style.error)}>
             <i aria-hidden className="fas fa-exclamation-circle"/>
--- src/components/Utils/Loader.tsx
+++ src/components/Utils/Loader.tsx
@@ -1,10 +1,19 @@
 import style from "./Loader.module.scss"
 import cs from "classnames"
 
-interface Props {}
+interface Props {
+  color?: "white" | "black"
+}
 
-export function Loader() {
+export function Loader({
+  color = "black"
+}: Props) {
   return (
-    <div className={cs(style.loader)}/>
+    <div 
+      className={cs(style.loader)}
+      style={{
+        backgroundColor: `var(--color-${color})`
+      }}
+    />
   )
 }
