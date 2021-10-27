2b49db2732f0b32ec76f8b22109dc5e854948779
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-10-27 16:55:26 +0200

    🐛 fixed typo in charset for rnd hash generation

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,2 +1,2 @@
-const charSet = 'abcdef0163456789'
+const charSet = 'abcdef0123456789'
 export const generateFxHash = () => Array(64).fill(0).map(_ => charSet[(Math.random() * charSet.length)|0]).join('')
