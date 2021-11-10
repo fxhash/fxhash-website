86c9fe303eb608e3102feb9268c0e7c177438ecd
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-11-10 20:04:17 +0200

    ✨ sandbox new architecture

---
+++ src/containers/Sandbox/Modes/SandboxEditor.tsx
@@ -0,0 +1,19 @@
+// import style from "./SandboxEditor.module.scss"
+import cs from "classnames"
+import { FunctionComponent } from "react"
+import { SandboxMode, SandboxModeProps, SandboxModule } from "./Generics"
+
+
+interface Props extends SandboxModeProps {
+}
+
+export const SandboxEditor: FunctionComponent<Props> = () => {
+  return (
+    <div>sandbox editor</div>
+  )
+}
+
+export const SandboxEditorModule: SandboxModule = {
+  mode: SandboxMode.EDITOR,
+  component: SandboxEditor
+}
