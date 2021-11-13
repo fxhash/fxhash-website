1c1604ef138338e0f0f7acea0fe99deb264f31c8
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-11-13 05:44:05 +0200

    ✏️ 12 -> 11

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: PropsWithChildren<{}>) {
     <>
       {process.env.NEXT_PUBLIC_BETA_MODE === "on" && (
         <Warning>
-          <span><strong>Warning</strong>: fx hash is in BETA mode. More about it by clicking this banner <strong>CONTRACTS WILL BE ENABLED at 12:00 CET, 13/11/2021</strong></span>
+          <span><strong>Warning</strong>: fx hash is in BETA mode. More about it by clicking this banner <strong>CONTRACTS WILL BE ENABLED at 11:00 CET, 13/11/2021</strong></span>
         </Warning>
       )}
 
