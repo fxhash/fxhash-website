98f0a21b441e6e3374a99dda430543050e5ac91b
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-11-13 05:32:07 +0200

    💬 contracts enabled update time

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: PropsWithChildren<{}>) {
     <>
       {process.env.NEXT_PUBLIC_BETA_MODE === "on" && (
         <Warning>
-          <span><strong>Warning</strong>: fx hash is in BETA mode. More about it by clicking this banner</span><strong> CONTRACTS TEMPORARILY DISABLED UNTIL SERVER UPGRADE</strong>
+          <span><strong>Warning</strong>: fx hash is in BETA mode. More about it by clicking this banner <strong>CONTRACTS WILL BE ENABLED at 12:00 CET, 13/11/2021</strong></span>
         </Warning>
       )}
 
