3e8f88cb853ee7d7e222acddf4ea08e4b413d336
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-12-19 13:43:02 +0100

    🐛 fixed dark mode not toggling

--- src/containers/Settings/Settings.tsx
+++ src/containers/Settings/Settings.tsx
@@ -12,22 +12,8 @@ export function Settings({
 }: Props) {
   const settings = useContext(SettingsContext)
 
-  const containerRef = useRef<HTMLDivElement>(null)
-  const [checked, setChecked] = useState(false)
-
-  useEffect(() => {
-    const listener = (evt: MouseEvent) => {
-      evt.stopPropagation()
-    }
-    containerRef.current?.addEventListener("click", listener)
-
-    return () => {
-      containerRef.current?.removeEventListener("click", listener)
-    }
-  }, [])
-
   return (
-    <div className={cs(style.root, className)} ref={containerRef}>
+    <div className={cs(style.root, className)}>
       <div className={cs(style.line)}>
         <strong>Dark mode</strong>
         <Switch 
