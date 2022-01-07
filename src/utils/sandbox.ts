f2ac04cbeb245846b929d0bdcd7d5d0151919530
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-01-07 18:51:27 +0100

    🐛 fixed sandbox not supporting SVG due to wrong MIME type

--- src/utils/sandbox.ts
+++ src/utils/sandbox.ts
@@ -43,6 +43,10 @@ export async function processZipSandbox(file: File): Promise<SandboxFiles> {
   // go through the files to create object URLS
   const record: SandboxFiles = {}
   for (const name in files) {
+    // if the file is a SVG file, we force the blob to take correct MIME type
+    if (name.slice(-4) === ".svg") {
+      files[name] = files[name].slice(0, files[name].size, "image/svg+xml")
+    }
     record[name] = {
       url: URL.createObjectURL(files[name])
     }
