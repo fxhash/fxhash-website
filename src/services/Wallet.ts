d72b30d6eaebae5d01dee6b1dd57977a225871c5
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-01-09 12:37:39 +0100

    ✨ 1/2 chances to have traffic on the first node of the list

--- src/services/Wallet.ts
+++ src/services/Wallet.ts
@@ -52,7 +52,16 @@ export class WalletManager {
   rpcNodes: string[]
 
   constructor() {
-    this.rpcNodes = shuffleArray((process.env.NEXT_PUBLIC_RPC_NODES!).split(','))
+    // !todo: REMOVE THE SHUFFLE once tests are done
+    // for now 1/2 of the traffic is going to go through the fxhash RPC endpoint
+    // to test if it works properly with some pretty high traffic
+    let RPCS = [...(process.env.NEXT_PUBLIC_RPC_NODES!).split(',')]
+    // 1/2 chances to shuffle the array, and so it's about 1/2 to always have the 
+    // fxhash RPC first
+    if (Math.random() < 0.5) {
+      RPCS = shuffleArray(RPCS)
+    }
+    this.rpcNodes = RPCS
     this.tezosToolkit = new TezosToolkit(this.rpcNodes[0])
     this.instanciateBeaconWallet()
   }
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,10 +1,10 @@
 export function shuffleArray<T = any>(array: T[]): T[] {
   const a = [...array]
   for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
   }
   return a
 }
