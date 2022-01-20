f283776e239d207cebdd3b813e37a02f37026830
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-01-21 00:41:33 +0100

    ✏️ ixart -> fxhash

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -32,7 +32,7 @@ const SandboxPage: NextPage = () => {
 
         <main className={cs(layout['padding-big'])}>
           <p>
-            The sandbox is a space in which you can drop a .zip of your project and see how it would behave when it will be minted on ixart. 
+            The sandbox is a space in which you can drop a .zip of your project and see how it would behave when it will be minted on fxhash. 
             If your artwork does not behave properly in the sandbox, it will not work after being minted. <br/>
             <span>If you are new to the platform please read our </span>
             <LinkGuide href="/articles/guide-mint-generative-token">
