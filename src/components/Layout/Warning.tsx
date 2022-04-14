f906dd6b64101a8b23ae526c9e9d65ef85118e8c
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-04-14 18:19:10 +0200

    📝 article v1.0

--- src/components/Layout/Warning.tsx
+++ src/components/Layout/Warning.tsx
@@ -3,11 +3,10 @@ import layout from "../../styles/Layout.module.scss"
 import cs from "classnames"
 import { FunctionComponent } from "react"
 import Link from "next/link"
-import { ContractsOpened } from "../Utils/ContractsOpened"
 
 export const Warning: FunctionComponent = ({ children }) => {
   return (
-    <Link href="/articles/beta">
+    <Link href="/doc/fxhash/one">
       <a className={cs(style.container)}>
         <span className={cs(style.message)}>
           <i aria-hidden className="fas fa-radiation-alt"/>
