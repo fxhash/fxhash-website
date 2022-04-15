3b43665268fd8c7692a41e1f5fd1b4afb8af0098
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-04-15 19:38:45 +0200

    💩

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,6 +1,8 @@
 // clamps a value x in the range [min, max]
 export const clamp = (x: number, min: number, max: number) => Math.max(min, Math.min(max, x))
 
+export const lerp = (a: number, b: number, t: number) => (b-a)*t + a
+
 export function getMutezDecimalsNb(x: number): number {
   const mu = Math.floor(Math.abs(x))
   const st = (mu/1000000).toString()
