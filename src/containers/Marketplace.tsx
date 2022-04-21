6bf9727a1ffab060a401db785de6ef1265d4d1fa
Davide Ciacco <davide.ciacco@neosperience.com>
2022-04-21 02:00:23 +0200

    load filters from url and use nextjs shallow routing to update the url

--- src/containers/Marketplace.tsx
+++ src/containers/Marketplace.tsx
@@ -18,6 +18,7 @@ import { ExploreTagDef, ExploreTags } from "../components/Exploration/ExploreTag
 import { displayMutez } from "../utils/units"
 import { SearchInputControlled } from "../components/Input/SearchInputControlled"
 import { Qu_listings } from "../queries/listing"
+import { useRouter } from "next/router"
 
 
 const ITEMS_PER_PAGE = 40
@@ -58,17 +59,66 @@ function sortValueToSortVariable(val: string) {
 }
 
 interface Props {
+  urlQuery: Record<string, string>
 }
 
-export const Marketplace = ({}: Props) => {
+const getFiltersFromUrlQuery = (urlQuery: Record<string, string>) => {
+  const {
+    search,
+    price_gte,
+    price_lte,
+    fullMint,
+    verified,
+    supply_lte,
+    supply_gte
+  } = urlQuery
+
+  // check all filters except for the search parameter
+  let p_lte = price_lte ? parseFloat(price_lte) : undefined
+  let p_gte = price_gte ? parseFloat(price_gte) : undefined
+  let ts_lte = supply_lte ? parseInt(supply_lte) : undefined
+  let ts_gte = supply_gte ? parseInt(supply_gte) : undefined
+  let loadedFilters: ListingFilters = {}
+  if (p_lte && p_lte !== NaN) loadedFilters.price_lte = p_lte.toString()
+  if (p_gte && p_gte !== NaN) loadedFilters.price_gte = p_gte.toString()
+  if (fullMint) loadedFilters.fullyMinted_eq = fullMint === "1"
+  if (verified) loadedFilters.authorVerified_eq = verified === "1"
+  if (ts_lte && ts_lte !== NaN) loadedFilters.tokenSupply_lte = ts_lte
+  if (ts_gte && ts_gte !== NaN) loadedFilters.tokenSupply_gte = ts_gte
+
+  // if there is a search query, add the searchQuery_eq filter and load the correct sort options
+  if (search) loadedFilters.searchQuery_eq = search
+
+  // set all loaded filters
+  return loadedFilters
+}
+
+const getSortFromUrlQuery = (urlQuery: Record<string, string>) => {
+  const { search, sort } = urlQuery
+
+  // if there is a sort value in the url, pre-select it in the sort input
+  // else, select the default value
+  let defaultSortValue = search ? 'relevance-desc' : 'createdAt-desc'
+  if (sort) {
+    let sortValues = search ? searchSortOptions : generalSortOptions
+    return sortValues.map(({ value }) => value).includes(sort)
+      ? sort
+      : defaultSortValue
+  }
+
+  return defaultSortValue
+}
+
+export const Marketplace = ({ urlQuery }: Props) => {
+
   // sort variables
-  const [sortValue, setSortValue] = useState<string>("createdAt-desc")
+  const [sortValue, setSortValue] = useState<string>(getSortFromUrlQuery(urlQuery))
   const sort = useMemo<Record<string, any>>(
-    () => sortValueToSortVariable(sortValue), 
+    () => sortValueToSortVariable(sortValue),
     [sortValue]
   )
   // sort options - when the search is triggered, options are updated to include relevance
-  const [sortOptions, setSortOptions] = useState<IOptions[]>(generalSortOptions)
+  const [sortOptions, setSortOptions] = useState<IOptions[]>(urlQuery.search ? searchSortOptions : generalSortOptions)
   // keeps track of the search option used before the search was triggered
   const sortBeforeSearch = useRef<string>(sortValue)
 
@@ -80,7 +130,7 @@ export const Marketplace = ({}: Props) => {
   }, [sortValue])
 
   // filters
-  const [filters, setFilters] = useState<ListingFilters>({})
+  const [filters, setFilters] = useState<ListingFilters>(getFiltersFromUrlQuery(urlQuery))
 
   // reference to an element at the top to scroll back
   const topMarkerRef = useRef<HTMLDivElement>(null)
@@ -99,8 +149,42 @@ export const Marketplace = ({}: Props) => {
     }
   })
 
+  const router = useRouter()
+  // update url parameters on filters changes
   useEffect(() => {
-    if (!loading) {
+
+    const {
+      price_gte,
+      price_lte,
+      fullyMinted_eq,
+      authorVerified_eq,
+      searchQuery_eq,
+      tokenSupply_lte,
+      tokenSupply_gte
+    } = filters
+
+    let query: any = {}
+
+    if (price_gte && price_gte !== 'NaN') query.price_gte = encodeURIComponent(price_gte)
+    if (price_lte && price_lte !== 'NaN') query.price_lte = encodeURIComponent(price_lte)
+    if (fullyMinted_eq !== undefined) query.fullMint = encodeURIComponent(fullyMinted_eq ? '1' : '0')
+    if (authorVerified_eq !== undefined) query.verified = encodeURIComponent(authorVerified_eq ? '1' : '0')
+    if (searchQuery_eq) query.search = encodeURIComponent(searchQuery_eq)
+    if (tokenSupply_lte && tokenSupply_lte !== NaN) query.supply_lte = encodeURIComponent(tokenSupply_lte)
+    if (tokenSupply_gte && tokenSupply_gte !== NaN) query.supply_gte = encodeURIComponent(tokenSupply_gte)
+
+    query.sort = encodeURIComponent(sortValue)
+
+    router.push(
+      { pathname: router.pathname, query },
+      `${router.pathname}?${Object.keys(query).map(key => `${key}=${query[key]}`).join('&')}`,
+      { shallow: true }
+    )
+
+  }, [filters, sortValue])
+
+  useEffect(() => {
+    if (!loading && data) {
       if (currentLength.current === data.listings?.length) {
         console.log("end current")
         ended.current = true
@@ -109,7 +193,7 @@ export const Marketplace = ({}: Props) => {
         currentLength.current = data.listings?.length
       }
     }
-  }, [loading])
+  }, [loading, data])
 
   const infiniteScrollFetch = () => {
     !ended.current && fetchMore?.({
@@ -143,11 +227,13 @@ export const Marketplace = ({}: Props) => {
     setFilters({
       ...filters,
       [filter]: value
-    })  
+    })
   }
 
   const removeFilter = (filter: string) => {
-    addFilter(filter, undefined)
+    let updatedFilters = { ...filters }
+    delete updatedFilters[filter as keyof ListingFilters]
+    setFilters(updatedFilters)
     // if the filter is search string, we reset the sort to what ti was
     if (filter === "searchQuery_eq" && sortValue === "relevance-desc") {
       setSortValue(sortBeforeSearch.current)
@@ -157,9 +243,10 @@ export const Marketplace = ({}: Props) => {
 
   // build the list of filters
   const filterTags = useMemo<ExploreTagDef[]>(() => {
+
     const tags: ExploreTagDef[] = []
     for (const key in filters) {
-      let value: string|null = null
+      let value: string | null = null
       // @ts-ignore
       if (filters[key] !== undefined) {
         switch (key) {
@@ -205,12 +292,12 @@ export const Marketplace = ({}: Props) => {
 
   return (
     <CardsExplorer>
-      {({ 
+      {({
         filtersVisible,
         setFiltersVisible,
       }) => (
         <>
-          <div ref={topMarkerRef}/>
+          <div ref={topMarkerRef} />
           <SearchHeader
             hasFilters
             filtersOpened={filtersVisible}
@@ -238,6 +325,7 @@ export const Marketplace = ({}: Props) => {
                   }
                 }
               }}
+              initialValue={urlQuery.search}
               className={styleSearch.large_search}
             />
           </SearchHeader>
@@ -252,7 +340,7 @@ export const Marketplace = ({}: Props) => {
               </FiltersPanel>
             )}
 
-            <div style={{width: "100%"}}>
+            <div style={{ width: "100%" }}>
               {filterTags.length > 0 && (
                 <>
                   <ExploreTags
@@ -263,7 +351,7 @@ export const Marketplace = ({}: Props) => {
                       setSortValue(sortBeforeSearch.current)
                     }}
                   />
-                  <Spacing size="regular"/>
+                  <Spacing size="regular" />
                 </>
               )}
 
@@ -274,7 +362,7 @@ export const Marketplace = ({}: Props) => {
               <InfiniteScrollTrigger onTrigger={infiniteScrollFetch} canTrigger={!!data && !loading}>
                 <CardsContainer>
                   {listings?.length > 0 && listings.map(offer => (
-                    <ObjktCard key={offer.id} objkt={offer.objkt}/>
+                    <ObjktCard key={offer.id} objkt={offer.objkt} />
                   ))}
                   {loading && (
                     <CardsLoading number={ITEMS_PER_PAGE} />
