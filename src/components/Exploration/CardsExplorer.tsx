a10b5f82c5bedcd3a1bcc945dfce075e0269260e
maerzhase <markus@lili16.de>
2022-04-27 19:18:55 +0200

    add cardSizeScope to CardsExplorer
    the cards explorer retrives the value based on the cardSizeScope
    and provides the value and the update function to the children.
    for now we dont store different scoped values in the context
    but a context must be provided otherwise the view will fallback
    to the default 270. cardsexplorer component also updates
    the css variable accordingly.
    
    - related to #63

--- src/components/Exploration/CardsExplorer.tsx
+++ src/components/Exploration/CardsExplorer.tsx
@@ -1,8 +1,7 @@
 import style from "./CardsExplorer.module.scss"
 import cs from "classnames"
-import { FunctionComponent, useState } from "react"
-
-
+import { FunctionComponent, useState, useEffect, useContext, useMemo } from "react"
+import { SettingsContext } from '../../context/Theme';
 /**
  * Component dedicated to holding the state of common explore pages (generative tokens, marketplace,
  * profile... etc)
@@ -17,21 +16,36 @@ interface PropsChildren {
 
 interface Props {
   filtersVisibleDefault?: boolean
+  cardSizeScope?: string
   children: FunctionComponent<PropsChildren>
 }
 export function CardsExplorer({
   filtersVisibleDefault = false,
+  cardSizeScope,
   children,
 }: Props) {
+
+  const settings = useContext(SettingsContext);
   // is the filters panel visible ?
   const [filtersVisible, setFiltersVisible] = useState<boolean>(filtersVisibleDefault)
   // is the search loading ?
   const [searchLoading, setSearchLoading] = useState<boolean>(false)
+  // get cardSize from scope or use default
+  const cardSize = useMemo<number>(
+    () => !cardSizeScope ? 270 : settings.cardSize,
+    [settings.cardSize, cardSizeScope]
+  )
+  useEffect(() => {
+    const root = document.documentElement
+    root.style.setProperty("--cards-size", `${cardSize}px`)
+  }, [cardSize])
 
   return children({
     filtersVisible,
     setFiltersVisible,
     searchLoading,
-    setSearchLoading
+    setSearchLoading,
+    cardSize,
+    setCardSize: (value) => settings.update('cardSize', value),
   })
 }
