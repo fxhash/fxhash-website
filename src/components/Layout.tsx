668accc3e88c51824ccfd8c77b54a84953354f2a
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-04-29 17:38:22 +0200

    Γ£¿ added button to add known libraries to splits (& their aliases fo the front)

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import style from "./Layout.module.scss"
+import { PropsWithChildren } from "react"
+import { Footer } from "../containers/Footer/Footer"
+import { Header } from "./Header"
+import { Warning } from "./Layout/Warning"
+
+export function Layout({ children }: PropsWithChildren<{}>) {
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_BETA_MODE === "on" && (
+        <Warning>
+          <span dangerouslySetInnerHTML={{ __html: process.env.NEXT_PUBLIC_BANNER_MESSAGE! }}/>
+        </Warning>
+      )}
+
+      <Header />
+
+      <main className={style.main}>
+        {children}
+      </main>
+
+      <Footer />
+    </>
+  )
+}
