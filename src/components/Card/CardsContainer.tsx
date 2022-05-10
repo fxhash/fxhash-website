c4aa81c3606f38cb765681b263f9567bbeadbec7
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-05-10 17:39:06 +0200

    🐛 always add extra divs to the cards container

--- src/components/Card/CardsContainer.tsx
+++ src/components/Card/CardsContainer.tsx
@@ -3,29 +3,23 @@ import cs from "classnames"
 import {forwardRef, HTMLAttributes, PropsWithChildren} from "react"
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
-  addExtraDivs?: boolean,
 }
 
 export const CardsContainer = forwardRef<HTMLDivElement, Props>(({
   children,
-  addExtraDivs,
   ...props
 }: PropsWithChildren<Props>, ref) => {
   return (
     <div {...props} className={cs(style.container, props.className)} ref={ref}>
       {children}
-      {addExtraDivs && (
-        <>
-          <div/>
-          <div/>
-          <div/>
-          <div/>
-          <div/>
-          <div/>
-          <div/>
-          <div/>
-        </>
-      )}
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+      <div/>
     </div>
   )
 });
