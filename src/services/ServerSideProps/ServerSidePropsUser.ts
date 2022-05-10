210a6fd227c329489a82f8f7da79342b86747c93
maerzhase <markus@lili16.de>
2022-05-10 22:01:03 +0200

    add randomization feature for enjoy

---
+++ src/services/ServerSideProps/ServerSidePropsUser.ts
@@ -0,0 +1,77 @@
+import { GetServerSideProps } from "next"
+import { Qu_user } from "../../queries/user"
+import { User, UserFlag, UserType } from "../../types/entities/User"
+import { isTezosAddress } from "../../utils/strings"
+import { UserAliases } from "../../utils/user"
+import client from "../ApolloClient"
+
+export const getServerSidePropsUserByName: GetServerSideProps = async (context) => {
+  const name = context.params?.name
+
+  // if there is an alias, query by ID instead
+  const userAlias = Object.values(UserAliases).find(alias => alias.name === name)
+  const variables = userAlias ? { id: userAlias.id } : { name }
+
+  let user = null
+
+  if (userAlias || (name && name.length > 0)) {
+    const { data, error } = await client.query({
+      query: Qu_user,
+      fetchPolicy: "no-cache",
+      variables
+    })
+    if (data) {
+      user = data.user
+    }
+  }
+  return {
+    props: {
+      user
+    },
+    notFound: !user
+  }
+}
+
+export const getServerSidePropsUserById: GetServerSideProps = async (context) => {
+  const pkh = context.params?.id
+  let user: User|null = null
+
+  if (pkh) {
+    const { data, error } = await client.query({
+      query: Qu_user,
+      fetchPolicy: "no-cache",
+      variables: { id: pkh }
+    })
+    if (data) {
+      user = data.user
+    }
+
+    // if there's no user in DB, but address is valid we craft a fake user object
+    // so that the page doesn't 404
+    if (!user && isTezosAddress(pkh as string)) {
+      const now = new Date().toISOString()
+      user = {
+        id: pkh as string,
+        description: "This account has not interacted with fxhsh contracts yet.",
+        type: UserType.REGULAR,
+        authorizations: [],
+        flag: UserFlag.NONE,
+        generativeTokens: [],
+        objkts: [],
+        offers: [],
+        actionsAsIssuer: [],
+        actionsAsTarget: [],
+        createdAt: now as any,
+        updatedAt: now as any,
+        collaborationContracts: [],
+      }
+    }
+  }
+
+  return {
+    props: {
+      user
+    },
+    notFound: !user
+  }
+}
