d129da40023a213d68ef7b1234b1c59a53f0c1ce
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-05-27 10:34:49 +0200

    ≡ƒÉ¢ sales table, bold active user

---
+++ src/components/TableUserSales/TableUserSales.tsx
@@ -0,0 +1,132 @@
+import React, { memo, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import style from "./TableUserSales.module.scss";
+import text from "../../styles/Text.module.css"
+import { Action } from "../../types/entities/Action";
+import { ActionReference } from "../Activity/Action";
+import { DisplayTezos } from "../Display/DisplayTezos";
+import { UserBadge } from "../User/UserBadge";
+import { ObjktImageAndName } from "../Objkt/ObjktImageAndName";
+import Skeleton from "../Skeleton";
+import { User } from '../../types/entities/User';
+import { Button } from "../Button";
+import cs from "classnames";
+import useHasScrolledToBottom from "../../hooks/useHasScrolledToBottom";
+import { UserContext } from '../../containers/UserProvider';
+
+interface TableUserSalesProps {
+  user: User,
+  sales: Action[],
+  loading?: boolean,
+  onScrollToBottom?: () => void,
+}
+const _TableUserSales = ({ user, sales, loading, onScrollToBottom }: TableUserSalesProps) => {
+  const { user: userInCtx } = useContext(UserContext)
+  const refWrapper = useRef<HTMLDivElement>(null);
+  const [hideTable, setHideTable] = useState<boolean>(false);
+  useHasScrolledToBottom(refWrapper, {
+    onScrollToBottom,
+    offsetBottom: 100
+  });
+  const handleToggleTableVisibility = useCallback(() => setHideTable(state => !state), []);
+  return (
+    <>
+      <div className={style['container-button']}>
+        <Button color="black" size="very-small" onClick={handleToggleTableVisibility}>
+          {hideTable ? 'Show' : 'Hide'}
+        </Button>
+      </div>
+      <div ref={refWrapper} className={cs(style.wrapper, {
+        [style.hide]: hideTable
+      })}>
+        <table className={style.table}>
+          <thead>
+          <tr>
+            <th className={style['th-gentk']}>Gentk</th>
+            <th className={style['th-price']}>Price</th>
+            <th className={style['th-user']}>Buyer</th>
+            <th className={style['th-user']}>Seller</th>
+            <th className={style['th-time']}>Time</th>
+          </tr>
+          </thead>
+          <tbody>
+          {(loading || sales.length > 0) ? sales.map(sale => (
+              <tr key={sale.id}>
+                <td className={style['td-gentk']}>
+                  {sale.objkt && (
+                    <div className={cs(style.link_wrapper)}>
+                      <ObjktImageAndName
+                        objkt={sale.objkt}
+                        imagePriority
+                      />
+                    </div>
+                  )}
+                </td>
+                <td className={style['td-price']}>
+                  <DisplayTezos
+                    className={style.price}
+                    formatBig={false}
+                    mutez={sale.numericValue}
+                    tezosSize="regular"
+                  />
+                </td>
+                <td className={style['td-user']}>
+                  {sale.issuer ? (
+                    <UserBadge
+                      hasLink
+                      user={sale.issuer}
+                      size="small"
+                      displayAvatar={false}
+                      className={cs({
+                        [text.bold]: userInCtx?.id === sale.issuer.id
+                      })}
+                    />
+                  ): <span>Unknown</span>}
+                </td>
+                <td className={style['td-user']}>
+                  {sale.target ? (
+                    <UserBadge
+                      hasLink
+                      user={sale.target}
+                      size="small"
+                      displayAvatar={false}
+                      className={cs({
+                        [text.bold]: userInCtx?.id === sale.target.id
+                      })}
+                    />
+                  ): <span>Unknown</span>}
+                </td>
+                <td className={style['td-time']}>
+                  <ActionReference action={sale} />
+                </td>
+              </tr>
+            )) :
+            <tr>
+              <td className={style.empty} colSpan={5}>
+                No sales found
+              </td>
+            </tr>
+          }
+          {loading && (
+            [...Array(29)].map((_, idx) => (
+              <tr key={idx}>
+                <td className={style['td-gentk']}>
+                  <div className={style['skeleton-wrapper']}>
+                    <Skeleton className={style['skeleton-thumbnail']} height="40px" width="40px"/>
+                    <Skeleton height="25px" width="100%"/>
+                  </div>
+                </td>
+                <td className={style['td-user']}><Skeleton height="25px"/></td>
+                <td className={style['td-user']}><Skeleton height="25px"/></td>
+                <td className={style['td-price']}><Skeleton height="25px"/></td>
+                <td className={style['td-time']}><Skeleton height="25px"/></td>
+              </tr>
+            ))
+          )}
+          </tbody>
+        </table>
+      </div>
+   </>
+  );
+}
+
+export const TableUserSales = memo(_TableUserSales);
---
+++ src/containers/Objkt/ObjktTabs.tsx
@@ -0,0 +1,53 @@
+import style from "./ObjktTabs.module.scss"
+import layout from "../../styles/Layout.module.scss"
+import cs from "classnames"
+import { useState } from "react"
+import { Objkt } from "../../types/entities/Objkt"
+import { Spacing } from "../../components/Layout/Spacing"
+import { TabsContainer } from "../../components/Layout/TabsContainer"
+import { Activity } from "../../components/Activity/Activity"
+import { ListOffers } from "../../components/List/ListOffers"
+import { SectionWrapper } from "../../components/Layout/SectionWrapper"
+
+interface Props {
+  objkt: Objkt
+}
+export function ObjktTabs({
+  objkt,
+}: Props) {
+  return (
+    <TabsContainer
+      tabDefinitions={[
+        {
+          name: "activity"
+        },
+        {
+          name: `offers (${objkt.offers?.length || 0})`
+        }
+      ]}
+      tabsLayout="fixed-size"
+    >
+      {({ tabIndex }) => (
+        <section className={cs(layout['padding-big'])}>    
+          <Spacing size="3x-large" />
+          {tabIndex === 0 ? (
+            <SectionWrapper layout="fixed-width-centered">
+              <Activity 
+                actions={objkt.actions}
+                className={cs(style.activity)}
+              />
+            </SectionWrapper>
+          ):tabIndex === 1 ? (
+            <SectionWrapper layout="fixed-width-centered">
+              <ListOffers
+                objkt={objkt}
+                offers={objkt.offers!}
+                className={cs(style.activity)}
+              />
+            </SectionWrapper>
+          ):null}
+        </section>
+      )}
+    </TabsContainer>
+  )
+}
