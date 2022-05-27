b639f0d302c295fa1cb01a28371ac2144a5ee87b
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-05-27 18:10:28 +0100

    fix(user): sales don't fetch more if there is nothing to fetch

--- src/containers/User/UserSales.tsx
+++ src/containers/User/UserSales.tsx
@@ -1,7 +1,5 @@
-import React, { memo, useCallback, useMemo } from 'react';
-import style from "./UserSales.module.scss";
+import React, { memo, useCallback, useMemo, useState } from 'react';
 import { User } from "../../types/entities/User";
-import cs from "classnames";
 import { useQuery } from "@apollo/client";
 import { Qu_userSales } from "../../queries/user";
 import { TableUserSales } from "../../components/Tables";
@@ -13,24 +11,28 @@ interface UserSalesTableProps {
 const ITEMS_PER_PAGE = 30
 
 const _UserSalesTable = ({ user }: UserSalesTableProps) => {
-  const { data, loading, fetchMore } = useQuery(Qu_userSales, {
+  const [hasNothingToFetch, setHasNothingToFetch] = useState(false);
+  const { data, loading, fetchMore } = useQuery<{ user: User }>(Qu_userSales, {
     notifyOnNetworkStatusChange: true,
     variables: {
       id: user.id,
       skip: 0,
       take: ITEMS_PER_PAGE,
-    }
+    },
   })
   const sales = useMemo(() => data?.user?.sales || [], [data?.user?.sales])
   const handleFetchMore = useCallback(async () => {
-    if (loading) return false;
-    await fetchMore({
+    if (loading || hasNothingToFetch) return false;
+    const { data } = await fetchMore({
       variables: {
         skip: sales.length,
         take: ITEMS_PER_PAGE
-      }
+      },
     });
-  }, [loading, fetchMore, sales.length])
+    if (!(data?.user.sales.length > 0)) {
+      setHasNothingToFetch(true);
+    }
+  }, [loading, hasNothingToFetch, fetchMore, sales.length])
   return (
     <div>
       <TableUserSales
