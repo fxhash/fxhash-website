458e7668bfbc71b1200a96265436959d35ef55dd
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-06-23 13:19:45 +0200

    feat(user): add draft style to article

--- src/components/Card/CardNFTArticleSkeleton.tsx
+++ src/components/Card/CardNFTArticleSkeleton.tsx
@@ -10,20 +10,22 @@ interface CardNftArticleSkeletonProps {
 const _CardNftArticleSkeleton = ({ className }: CardNftArticleSkeletonProps) => {
   return (
     <div className={cs(style.container, className)}>
-      <div className={style['img-wrapper']}>
-        <Skeleton height="100%" width="100%" />
+      <div className={style.content}>
+        <div className={style['img-wrapper']}>
+          <Skeleton height="100%" width="100%" />
+        </div>
+        <div className={style.infos}>
+          <div className={style.infos_header}>
+            <Skeleton height="37px" width="250px" />
+          </div>
+          <Skeleton height="33px" width="350px" />
+          <div className={style.description}>
+            <Skeleton height="80%" />
+          </div>
+          <div className={style.tags}>
+            {[1, 2, 3].map(nb => <Skeleton key={nb} height="29px" width="84px" />)}
+          </div>
       </div>
-      <div className={style.infos}>
-        <div className={style.infos_header}>
-          <Skeleton height="37px" width="250px" />
-        </div>
-        <Skeleton height="33px" width="350px" />
-        <div className={style.description}>
-          <Skeleton height="80%" />
-        </div>
-        <div className={style.tags}>
-          {[1, 2, 3].map(nb => <Skeleton key={nb} height="29px" width="84px" />)}
-        </div>
       </div>
     </div>
   );
