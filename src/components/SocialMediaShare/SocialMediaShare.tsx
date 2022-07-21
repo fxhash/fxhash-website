60775946c015361734df6679c1d9876b6334aa57
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-07-21 10:55:24 +0100

    merge

---
+++ src/components/SocialMediaShare/SocialMediaShare.tsx
@@ -0,0 +1,37 @@
+import React, { memo, useMemo } from 'react';
+import style from "./SocialMediaShare.module.scss";
+
+type SocialMediaShareProps = {
+  url: string
+}
+
+const SocialMediaShare = ({ url }: SocialMediaShareProps) => {
+  const socialMedias = useMemo(() => {
+    const sharingUrl = encodeURIComponent(url);
+    const fbUrl = `https://www.facebook.com/sharer/sharer.php?u=${sharingUrl}`;
+    const twUrl = `https://twitter.com/intent/tweet?url=${sharingUrl}`;
+    const lnUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${sharingUrl}`;
+
+    return [
+      { key: 'twitter', url: twUrl, icon: 'fa-brands fa-twitter' },
+      { key: 'fb', url: fbUrl, icon: 'fa-brands fa-facebook' },
+      { key: 'linkedin', url: lnUrl, icon: 'fa-brands fa-linkedin' },
+    ];
+  }, [url]);
+  return (
+      <div className={style.container}>
+        {socialMedias.map((media) => (
+          <a
+            key={media.key}
+            href={media.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i aria-hidden className={media.icon} />
+          </a>
+        ))}
+      </div>
+  );
+};
+
+export default memo(SocialMediaShare);
