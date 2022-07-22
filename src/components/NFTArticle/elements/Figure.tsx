56ef4b9a8d3565acdf6d5ee91e38acd9aa1aa30d
maerzhase <markus@lili16.de>
2022-07-22 10:11:03 +0200

    remove manual focus handler on figure

--- src/components/NFTArticle/elements/Figure.tsx
+++ src/components/NFTArticle/elements/Figure.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useRef } from "react"
+import { PropsWithChildren, useEffect, useState } from "react"
 import ReactTextareaAutosize from "react-textarea-autosize"
 import { Transforms, Node } from "slate"
 import { ReactEditor, useFocused, useSelected, useSlateStatic } from "slate-react"
@@ -17,18 +17,9 @@ export function FigureElement({
 }: PropsWithChildren<Props>) {
   const selected = useSelected()
   const focused = useFocused()
-  const editor = useSlateStatic();
-
-  const handleFocus = () => {
-    const slateNode = ReactEditor.toSlateNode(editor, attributes.ref.current);
-    const path = ReactEditor.findPath(editor, slateNode)
-    const captionText = Node.string(slateNode)
-    const point = {path: [...path, 1], offset: captionText.length }
-    Transforms.select(editor, point)
-  }
-
+  
   return (
-    <figure {...attributes} onFocus={handleFocus} tabIndex="0" className={cs(style.figure, {
+    <figure {...attributes} className={cs(style.figure, {
       [style.selected]: selected && focused
     })}>
       {children}
