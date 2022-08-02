33cf15cb345dd847b6afae1247fbd23040eda6ec
maerzhase <markus@lili16.de>
2022-08-02 19:31:23 +0200

    make softLineBreak plugin extensible

--- src/components/NFTArticle/SlateEditor/Plugins/SoftBreakPlugin.ts
+++ src/components/NFTArticle/SlateEditor/Plugins/SoftBreakPlugin.ts
@@ -1,11 +1,26 @@
 import { EnhanceEditorWith } from "../../../../types/ArticleEditor/Editor";
+import { getBlockElementAtSelection } from "../utils";
+
+const BLOCK_TYPES_WITH_SOFT_LINEBREAK = [
+  'paragraph',
+  'blockquote',
+  'list',
+  'table',
+  'heading',
+]
 
 export const withSoftBreak: EnhanceEditorWith = (editor) => {
-  const { insertText } = editor
+  const { insertText, insertSoftBreak } = editor
 
   editor.insertSoftBreak = () => {
-    insertText('\n')
-    return true;
+    if(editor.selection) {
+      const [element] = getBlockElementAtSelection(editor, editor.selection)
+      if (element && BLOCK_TYPES_WITH_SOFT_LINEBREAK.includes(element.type)) {
+	insertText('\n')
+	return true;
+      }
+    }
+    insertSoftBreak();
   }
 
   return editor
