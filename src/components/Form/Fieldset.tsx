ddfc48bc31cc17aabba6d80e3b2060223b7b0d9e
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-08-05 22:05:24 +0200

    Merge branch 'feature/articles' into feature/articles-hr

---
+++ src/components/Form/Fieldset.tsx
@@ -0,0 +1,29 @@
+import style from "./Form.module.scss"
+import cs from "classnames"
+import { PropsWithChildren } from "react"
+
+interface Props {
+  className?: string
+  error?: string
+	errorPos?: "top-right" | "bottom-left"
+}
+
+export function Fieldset({
+  className,
+  error,
+  errorPos = "top-right",
+  children,
+}: PropsWithChildren<Props>) {
+  return (
+    <fieldset className={cs(style.fieldset, className, {
+      [style.field_error]: !!error,
+    })}>
+      {error && (
+        <div className={cs(style.error, style[`error-${errorPos}`])}>
+          {error}
+        </div>
+      )}
+      {children}
+    </fieldset>
+  )
+}
