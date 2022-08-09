6254c2bf2120d99d50c75aa59c48633757282a74
Markus Maerzhase <maerzhase@users.noreply.github.com>
2022-08-09 21:49:42 +0200

    Merge branch 'feature/articles' into fix/#232-add-shortcut-for-creating-links

---
+++ src/components/Link/LinkGuide.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react"
+import { LinkIcon } from "./LinkIcon"
+
+
+interface Props {
+  href: string
+  newTab?: boolean
+}
+
+export const LinkGuide: FunctionComponent<Props> = ({
+  href,
+  newTab,
+  children
+}) => {
+  return (
+    <LinkIcon
+      iconComp={<i aria-hidden className="fas fa-book"/>}
+      href={href}
+      newTab={newTab}
+    >
+      {children}
+    </LinkIcon>
+  )
+}
---
+++ src/containers/Article/Edition/ArticleEdition.tsx
@@ -0,0 +1,85 @@
+import layout from "../../../../styles/Layout.module.scss"
+import cs from "classnames"
+import { GetServerSideProps, NextPage } from "next"
+import Head from "next/head"
+import { ArticleEditor } from "../../../containers/Article/Editor/ArticleEditor"
+import { Spacing } from "../../../components/Layout/Spacing"
+import React, { FunctionComponent, useCallback, useContext, useEffect, useState } from "react"
+import { ArticlesContext } from "../../../context/Articles"
+import { useRouter } from "next/router"
+import { LoaderBlock } from "../../../components/Layout/LoaderBlock"
+import useInit from "../../../hooks/useInit"
+import { NFTArticle } from "../../../types/entities/Article"
+import { generateNFTArticleDraft } from "../../../utils/nft-article"
+import { NFTArticleForm } from "../../../types/ArticleEditor/Editor"
+
+interface Props {
+  article: NFTArticle
+  origin?: string
+}
+const ArticleEdition: FunctionComponent<Props> = ({
+  article,
+  origin,
+}) => {
+  const [hasLoadUpToDate, setHasLoadUpToDate] = useState(false)
+  const router = useRouter()
+  const { state, dispatch } = useContext(ArticlesContext)
+  const id = ""+article!.id
+
+  const handleSubmit = useCallback((values) => {
+    dispatch({
+      type: 'save',
+      payload: { 
+        id: id,
+        articleForm: values,
+        minted: true,
+      }
+    })
+    router.push(`/article/editor/${id}/preview`)
+  }, [dispatch, router])
+
+  useInit(() => {
+    dispatch({ type: 'loadAll' })
+    setHasLoadUpToDate(true)
+  })
+
+  // check if an article with the same ID exists locally, if not create a new
+  // draft article in the memory with the same ID
+  useEffect(() => {
+    if (hasLoadUpToDate) {
+      // if no article in the state, instanciate a draft
+      if (!state.articles[id]) {
+        const draft = generateNFTArticleDraft(article!)
+        dispatch({
+          type: "save",
+          payload: {
+            id: id,
+            articleForm: draft as NFTArticleForm,
+            minted: true,
+          }
+        })
+      }
+    }
+  }, [hasLoadUpToDate])
+
+  // pull the draft from the articles local state
+  const localDraft = state.articles[id] || null
+  const ready = hasLoadUpToDate && router.isReady && localDraft
+
+  return ready ? (
+    <ArticleEditor
+      initialValues={localDraft.form}
+      hasLocalAutosave
+      localId={id}
+      onSubmit={handleSubmit}
+      editMinted
+    />
+  ):(
+    <LoaderBlock
+      size="small"
+      height="60vh"
+    />
+  )
+}
+
+export default ArticleEdition
