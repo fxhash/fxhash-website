2b2fd4682bd251e2666f0b5dbde5660734cdbd1a
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-08-18 20:15:04 +0200

    ≡ƒÜ¿ fix compiler warning

---
+++ src/containers/Objkt/OfferCancel.tsx
@@ -0,0 +1,44 @@
+import { Button } from "../../components/Button"
+import { Objkt } from "../../types/entities/Objkt"
+import { ContractFeedback } from "../../components/Feedback/ContractFeedback"
+import { displayMutez } from "../../utils/units"
+import { useContractOperation } from "../../hooks/useContractOperation"
+import { Offer } from "../../types/entities/Offer"
+import { OfferCancelOperation } from "../../services/contract-operations/OfferCancel"
+
+interface Props {
+  offer: Offer
+  objkt: Objkt
+}
+
+export function OfferCancel({ offer, objkt }: Props) {
+  const { state, loading: contractLoading, error: contractError, success, call, clear } = 
+    useContractOperation(OfferCancelOperation)
+
+  const callContract = () => {
+    call({
+      offer: offer,
+      objkt: objkt,
+    })
+  }
+
+  return (
+    <>
+      <ContractFeedback
+        state={state}
+        loading={contractLoading}
+        success={success}
+        error={contractError}
+        successMessage="Your offer has been cancelled"
+      />
+
+      <Button
+        state={contractLoading ? "loading" : "default"}
+        color="primary"
+        onClick={callContract}
+      >
+        cancel offer ({displayMutez(offer.price)} tez)
+      </Button>
+    </>
+  )
+}
---
+++ src/context/Articles.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useMemo, useReducer, useState } from "react"
+import { DraftNFTArticle, NFTArticleForm } from "../types/ArticleEditor/Editor";
+
+interface ArticlesState {
+  articles: {
+    [key: string]: DraftNFTArticle | null,
+  }
+}
+interface ISavePayload {
+  id: string
+  articleForm: NFTArticleForm
+  minted?: boolean
+}
+type ArticlesAction =
+  | { type: "loadAll" }
+  | { type: "save", payload: ISavePayload }
+  | { type: "delete", payload: { id: string } }
+interface Context {
+  state: ArticlesState
+  dispatch: React.Dispatch<ArticlesAction>
+  // check if an article is being edited
+  isEdited: (id: string) => boolean
+}
+
+const initialState: ArticlesState = { 
+  articles: {}
+}
+const localStorageKey = 'local_articles';
+export const ArticlesContext = React.createContext<Context>({} as Context)
+
+const loadAllLocalArticles = () => {
+  const localArticlesValue = localStorage.getItem(localStorageKey);
+  return localArticlesValue ? JSON.parse(localArticlesValue) : initialState;
+};
+const articlesReducer = (state: ArticlesState, action: ArticlesAction): ArticlesState => {
+  switch (action.type) {
+    case "loadAll":
+      return loadAllLocalArticles();
+    case "save": {
+      const newState = {...state} || loadAllLocalArticles();
+      newState.articles[action.payload.id] = {
+        form: action.payload.articleForm,
+        lastSavedAt: new Date().toUTCString(),
+        minted: action.payload.minted || false,
+      };
+      localStorage.setItem(localStorageKey, JSON.stringify(newState));
+      return newState;
+    }
+    case "delete": {
+      const newState = loadAllLocalArticles();
+      delete newState.articles[action.payload.id];
+      localStorage.setItem(localStorageKey, JSON.stringify(newState));
+      return newState;
+    }
+    default:
+      return state;
+  }
+}
+interface ArticlesProviderProps {
+  children: any;
+}
+export const ArticlesProvider = ({ children }: ArticlesProviderProps) => {
+  const [state, dispatch] = useReducer<React.Reducer<ArticlesState, ArticlesAction>>(articlesReducer, initialState)
+  const providerValue = useMemo(
+    () => ({
+      state,
+      dispatch,
+      isEdited: (id: string) => !!state.articles[id],
+    }),
+    [state, dispatch],
+  )
+
+  useEffect(() => {
+    dispatch({ type: "loadAll" })
+  }, [dispatch])
+
+  return <ArticlesContext.Provider value={providerValue}>
+    {children}
+  </ArticlesContext.Provider>
+}
