f000fa833292b5f7070ada9254bdb29010bc4343
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-08-19 17:19:17 +0200

    Γ£¿ add mentions on the Generative Token page

---
+++ src/components/Utils/InfiniteScrollTrigger.tsx
@@ -0,0 +1,35 @@
+import style from "./InfiniteScrollTrigger.module.scss"
+import cs from "classnames"
+import { PropsWithChildren, useEffect, useRef } from "react"
+import { useInView } from "react-intersection-observer"
+
+
+interface Props {
+  canTrigger?: boolean
+  onTrigger: () => void
+  className?: string
+}
+
+export function InfiniteScrollTrigger({
+  canTrigger = true,
+  onTrigger,
+  className,
+  children
+}: PropsWithChildren<Props>) {
+  const { ref, inView, entry } = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      if (canTrigger) {
+        onTrigger()
+      }
+    }
+  }, [inView, canTrigger, onTrigger])
+
+  return (
+    <div className={cs(style.container, className)}>
+      {children}
+      <div className={cs(style.trigger)} ref={ref} />
+    </div>
+  )
+}
