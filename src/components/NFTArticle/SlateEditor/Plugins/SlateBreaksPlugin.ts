4e489f20115a04c31a95a26f4b7e9fce54b424d0
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-09-01 12:19:06 +0100

    fix(article): remove entire nodes when deleting foward/backward for medias (figure) and table

---
+++ src/components/NFTArticle/SlateEditor/Plugins/SlateBreaksPlugin.ts
@@ -0,0 +1,57 @@
+import { Transforms, Path, NodeEntry, Node } from "slate";
+import { EnhanceEditorWith, FxEditor } from "../../../../types/ArticleEditor/Editor";
+import { lookupElementAtSelection } from "../utils";
+import { getArticleBlockDefinition } from "../Blocks";
+
+export enum EBreakBehavior {
+  "default" = "default",
+  "insertParagraph" = "insertParagraph",
+  "insertParagraphIfEmpty" = "insertParagraphIfEmpty",
+  "nothing" = "nothing",
+}
+
+export type ShouldDefaultInsertBreak = boolean;
+export type InsertBreakFunction = (editor: FxEditor, element: NodeEntry) => ShouldDefaultInsertBreak | void
+export const breakBehaviors: Record<EBreakBehavior, InsertBreakFunction> = {
+  default: () => true,
+  insertParagraph: (editor, element) => {
+    const [, pathEl] = element;
+    const pathNextEl = Path.next(pathEl)
+    Transforms.splitNodes(editor, { always: true });
+    Transforms.setNodes(editor, { type: 'paragraph'}, {
+      at: pathNextEl,
+    });
+  },
+  insertParagraphIfEmpty: (editor, element) => {
+    const [node] = element;
+    const text = Node.string(node);
+    if (text) return true;
+    breakBehaviors.insertParagraph(editor, element)
+  },
+  nothing: () => {},
+}
+
+/**
+ * Manage break behavior of elements
+ */
+export const withBreaks: EnhanceEditorWith = (editor) => {
+  const { insertBreak } = editor
+
+  editor.insertBreak = () => {
+    const { selection } = editor;
+    const nodeEntry = lookupElementAtSelection(editor, selection);
+    if (nodeEntry) {
+      const [node] = nodeEntry;
+      const definition = getArticleBlockDefinition(node.type);
+      const behavior = definition.insertBreakBehavior;
+      if (behavior) {
+        const insertBreakFunction = typeof behavior === 'function' ? behavior : breakBehaviors[behavior]
+        const shouldDefaultInsertBreak = insertBreakFunction(editor, nodeEntry);
+        if (!shouldDefaultInsertBreak) return;
+      }
+    }
+    insertBreak();
+  }
+
+  return editor
+}
---
+++ src/utils/nft-article.ts
@@ -0,0 +1,68 @@
+import { ArticleFlag, NFTArticle } from "../types/entities/Article";
+import { DraftNFTArticle, NFTArticleForm } from "../types/ArticleEditor/Editor";
+import { User } from "../types/entities/User";
+
+type GenerateNftArticleFromDraft = (id: string, draft: DraftNFTArticle, user?: User) => NFTArticle;
+export const generateNftArticleFromDraft: GenerateNftArticleFromDraft = (id, draft, user) => {
+  return ({
+    id,
+    slug: `preview-${id}`,
+    flag: ArticleFlag.NONE,
+    author: user,
+    ledgers: [],
+    generativeTokenJointures: [],
+    revisions: [],
+    royaltiesSplits: [], // draft.form.royaltiesSplit, // transform
+    actions: [],
+    title: draft.form.title,
+    description: draft.form.abstract,
+    body: draft.form.body,
+    tags: draft.form.tags,
+    language: 'en',
+    metadataUri: '',
+    relatedArticles: [],
+    metadata: {
+      decimals: 0,
+      symbol: "ARTKL",
+      name: draft.form.title,
+      description: draft.form.abstract,
+      minter: user?.id,
+      creators: [],
+      contributors: [],
+      type: "article",
+      tags: draft.form.tags,
+      language: 'en',
+      artifactUri: '',
+      displayUri: draft.form.thumbnailUri || '',
+      thumbnailUri: draft.form.thumbnailUri || '',
+      thumbnailCaption: draft.form.thumbnailCaption,
+      platforms: []
+    },
+    metadataLocked: false,
+    artifactUri: '',
+    displayUri: draft.form.thumbnailUri || '',
+    thumbnailUri: draft.form.thumbnailUri || '',
+    thumbnailCaption: draft.form.thumbnailCaption || '',
+    platforms: null,
+    createdAt: draft.lastSavedAt,
+    editions: parseInt(draft.form.editions),
+    royalties: parseFloat(draft.form.royalties) * 10,
+    mintOpHash: ''
+  })
+}
+
+/**
+ * Given a NFTArticle entity, generates a draft for its edition
+ */
+export function generateNFTArticleDraft(
+  article: NFTArticle
+): Partial<NFTArticleForm> {
+  return {
+    title: article.title,
+    thumbnailUri: article.displayUri,
+    thumbnailCaption: article.thumbnailCaption,
+    body: article.body,
+    abstract: article.description,
+    tags: article.tags,
+  }
+}
