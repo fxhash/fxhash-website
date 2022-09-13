3a069902b623f9c70f174298b266cc10ceb7ea05
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-09-13 18:15:54 +0200

    ✨ hide warning on live minting context

--- src/containers/LiveMinting/LiveMintingRoot.tsx
+++ src/containers/LiveMinting/LiveMintingRoot.tsx
@@ -1,6 +1,7 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useContext, useEffect } from "react"
 import { LiveMintingWalletGuard } from "../../components/LiveMinting/WalletGuard"
 import { LiveMintingProvider } from "../../context/LiveMinting"
+import { MessageCenterContext } from "../../context/MessageCenter"
 import { LiveMintingGuard } from "./LiveMintingGuard"
 
 type Props = PropsWithChildren<{
@@ -8,6 +9,14 @@ type Props = PropsWithChildren<{
 export function LiveMintingRoot({
   children,
 }: Props) {
+  // we hide the warnings in the message center to make a better flow
+  const messageCenter = useContext(MessageCenterContext)
+  useEffect(() => {
+    if (messageCenter.ready) {
+      messageCenter.setIgnoreWarnings(true)
+    }
+  }, [messageCenter.ready])
+
   return (
     <LiveMintingProvider>
       <LiveMintingGuard>
