524352d525835476d19b552de91e7c96a05d19d2
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-09-16 15:03:01 +0200

    fix(import): add more space to import list

---
+++ src/queries/articles.ts
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+import { Frag_ArticleFull, Frag_ArticleInfos } from "./fragments/article";
+import { Frag_ListingArticle } from "./fragments/listing";
+
+export const Qu_articles = gql`
+  query Articles($filters: ArticleFilter, $sort: ArticleSortInput, $skip: Int, $take: Int) {
+    articles(filters: $filters, sort: $sort, skip: $skip, take: $take) {
+      ...ArticleInfos
+    }
+  }
+  ${Frag_ArticleInfos}
+`;
+export const Qu_articleBySlug = gql`
+  query ArticleBySlug($slug: String!) {
+    article(slug: $slug) {
+      ...ArticleFull
+    }
+  }
+  ${Frag_ArticleFull}
+`;
+export const Qu_articleById = gql`
+  query ArticleById($id: Int!) {
+    article(id: $id) {
+      ...ArticleFull
+    }
+  }
+  ${Frag_ArticleFull}
+`;
+export const Qu_articleActionsById = gql`
+  query ArticleActionsById($id: Int!) {
+    article(id: $id) {
+      id
+      title
+      ledger {
+        amount
+        owner {
+          id
+          name
+          avatarUri
+          flag
+        }
+      }
+      activeListings {
+        ...ListingArticle
+      }
+    }
+  }
+  ${Frag_ListingArticle}
+`
+
+export const Qu_articleListingsById = gql`
+  query ArticleActionsById($id: Int!) {
+    article(id: $id) {
+      id
+      activeListings {
+        ...ListingArticle
+      }
+    }
+  }
+  ${Frag_ListingArticle}
+`
