0ac349561f2f62c6b47ee2bea1bea5ba86d66103
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-09-23 16:21:28 +0200

    fix(article): remove 'soon to be collected' copy in numbers of editions

---
+++ src/queries/fragments/generative-token.ts
@@ -0,0 +1,87 @@
+import { gql } from "@apollo/client";
+import { Frag_ArticleInfos } from "./article";
+
+export const Frag_GenPricing = gql`
+  fragment Pricing on GenerativeToken {
+    pricingFixed {
+      price
+      opensAt
+    }
+    pricingDutchAuction {
+      levels
+      restingPrice
+      finalPrice
+      decrementDuration
+      opensAt
+    }
+  }
+`
+
+export const Frag_GenAuthor = gql`
+  fragment Author on GenerativeToken {
+    author {
+      id
+      name
+      type
+      avatarUri
+      flag
+      collaborators {
+        id
+        name
+        avatarUri
+        flag
+      }
+    }
+  }
+`
+
+export const Frag_GenSplitsPrimary = gql`
+  fragment SplitsPrimary on GenerativeToken {
+    splitsPrimary {
+      pct
+      user {
+        id
+        name
+        avatarUri
+        flag
+      }
+    }
+  }
+`
+
+export const Frag_GenSplitsSecondary = gql`
+  fragment SplitsSecondary on GenerativeToken {
+    splitsSecondary {
+      pct
+      user {
+        id
+        name
+        avatarUri
+        flag
+      }
+    }
+  }
+`
+
+export const Frag_GenReserves = gql`
+  fragment Reserves on GenerativeToken {
+    reserves {
+      data
+      amount
+      method
+    }
+  }
+`
+
+export const Frag_GenArticleMentions = gql`
+  ${Frag_ArticleInfos}
+
+  fragment ArticleMentions on GenerativeToken {
+    articleMentions {
+      line
+      article {
+        ...ArticleInfos
+      }
+    }
+  }
+`
