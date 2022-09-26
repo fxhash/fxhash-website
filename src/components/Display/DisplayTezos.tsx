d9ebf7f49ae155b4a91df206f49fa7164941df94
maerzhase <markus@lili16.de>
2022-09-26 12:23:32 +0200

    fix build errors

---
+++ src/components/Display/DisplayTezos.tsx
@@ -0,0 +1,24 @@
+import style from "./DisplayTezos.module.scss"
+import cs from "classnames"
+import { bigMutezFormatter, displayMutez } from "../../utils/units"
+import { IconTezos } from "../Icons/IconTezos"
+
+interface Props {
+  mutez: number
+  className?: string
+  formatBig?: boolean
+  tezosSize?: "small" | "regular" | "big"
+}
+export function DisplayTezos({
+  mutez,
+  className,
+  formatBig = true,
+  tezosSize = "small",
+}: Props) {
+  return (
+    <span className={cs(style.root, className)}>
+      <IconTezos size={tezosSize} />
+      <span>{formatBig ? bigMutezFormatter(mutez) : displayMutez(mutez)}</span>
+    </span>
+  )
+}
---
+++ src/components/NFTArticle/processor/getSlateEditorStateFromMarkdown.ts
@@ -0,0 +1,97 @@
+import { Descendant } from "slate"
+import matter from "gray-matter"
+import { unified } from "unified"
+import remarkParse from "remark-parse"
+import remarkMath from "remark-math"
+import remarkUnwrapImages from "remark-unwrap-images"
+import remarkDirective from "remark-directive"
+import { remarkToSlate } from "remark-slate-transformer"
+import { OverridedMdastBuilders } from "remark-slate-transformer/lib/transformers/mdast-to-slate"
+import {
+  mdastFlattenListItemParagraphs,
+  mdastParseMentions,
+  remarkFxHashCustom,
+} from "./plugins"
+import remarkGfm from "remark-gfm"
+import { mathProcessor } from "../elements/Math/MathProcessor"
+import { imageProcessor } from "../elements/Image/ImageProcessor"
+import { videoProcessor } from "../elements/Video/VideoProcessor"
+import { mentionProcessor } from "../elements/Mention/MentionProcessor"
+
+interface DirectiveNodeProps {
+  [key: string]: any
+}
+
+const directives: Record<string, (node: any) => object> = {
+  video: videoProcessor.transformMarkdownMdhastToSlate!,
+}
+
+function createDirectiveNode(
+  node: any,
+  next: (children: any[]) => any
+): object {
+  const data = node.data || {}
+  const hProperties: { [key: string]: any } = (data.hProperties || {}) as {
+    [key: string]: any
+  }
+  // extract only defined props to avoid error serialization of undefined
+  const propertiesWithoutUndefined: DirectiveNodeProps = Object.keys(
+    hProperties
+  ).reduce((acc: DirectiveNodeProps, key: string) => {
+    const value = hProperties[key]
+    if (value) {
+      acc[key] = value
+    }
+    return acc
+  }, {})
+  const newNode = {
+    type: data.hName,
+    children: next(node.children),
+    ...propertiesWithoutUndefined,
+  }
+  const instanciateNode = directives[newNode.type]
+  return instanciateNode ? instanciateNode(newNode) : newNode
+}
+const remarkSlateTransformerOverrides: OverridedMdastBuilders = {
+  textDirective: createDirectiveNode,
+  leafDirective: createDirectiveNode,
+  containerDirective: createDirectiveNode,
+  inlineMath: mathProcessor.transformMarkdownMdhastToSlate,
+  math: mathProcessor.transformMarkdownMdhastToSlate,
+  image: imageProcessor.transformMarkdownMdhastToSlate,
+  mention: mentionProcessor.transformMarkdownMdhastToSlate,
+}
+
+interface PayloadSlateEditorStateFromMarkdown {
+  [p: string]: any
+  editorState: Descendant[]
+}
+
+export default async function getSlateEditorStateFromMarkdown(
+  markdown: string
+): Promise<PayloadSlateEditorStateFromMarkdown | null> {
+  try {
+    const matterResult = matter(markdown)
+    const processed = await unified()
+      .use(remarkParse)
+      .use(mdastFlattenListItemParagraphs)
+      .use(mdastParseMentions)
+      .use(remarkMath)
+      .use(remarkGfm)
+      .use(remarkUnwrapImages)
+      .use(remarkDirective)
+      .use(remarkFxHashCustom)
+      .use(remarkToSlate, {
+        overrides: remarkSlateTransformerOverrides,
+      })
+      .process(matterResult.content)
+
+    return {
+      ...matterResult.data,
+      editorState: processed.result as Descendant[],
+    }
+  } catch (e) {
+    console.error(e)
+    return null
+  }
+}
