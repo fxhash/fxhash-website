f4ded0be91539aba0fc7bea1b61c9b323cd38a82
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-10-03 17:31:02 +0200

    fix(article): detect incognito package switch to latest fix build

--- src/containers/Article/Editor/AutosaveArticle.tsx
+++ src/containers/Article/Editor/AutosaveArticle.tsx
@@ -6,7 +6,7 @@ import React, {
   useMemo,
   useState,
 } from "react"
-import { detectIncognito } from "detect-incognito"
+import { detectIncognito } from "detectincognitojs"
 import style from "./AutosaveArticle.module.scss"
 import effects from "../../../styles/Effects.module.scss"
 import text from "../../../styles/Colors.module.css"
