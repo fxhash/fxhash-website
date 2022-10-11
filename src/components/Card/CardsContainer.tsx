05905ce2b7373ac55e10a384143dad5c27b036ed
maerzhase <markus@lili16.de>
2022-10-11 12:36:42 +0200

    set grid template style when size provided

--- src/components/Card/CardsContainer.tsx
+++ src/components/Card/CardsContainer.tsx
@@ -2,15 +2,22 @@ import style from "./CardsContainer.module.scss"
 import cs from "classnames"
 import { forwardRef, HTMLAttributes, PropsWithChildren } from "react"
 
-interface Props extends HTMLAttributes<HTMLDivElement> {}
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  cardSize?: number
+}
 
 export const CardsContainer = forwardRef<HTMLDivElement, Props>(
-  ({ children, ...props }: PropsWithChildren<Props>, ref) => {
+  ({ children, cardSize, ...props }: PropsWithChildren<Props>, ref) => {
     return (
       <div
         {...props}
         className={cs(style.container, props.className)}
         ref={ref}
+        style={{
+          gridTemplateColumns:
+            cardSize && `repeat(auto-fit, minmax(${cardSize}px, 1fr))`,
+          ...props?.style,
+        }}
       >
         {children}
         <div />
