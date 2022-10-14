2884c1f36eb5318257dbe8a44122b4d4c083a2e6
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-10-14 16:57:39 +0200

    Merge branch 'dev' into feat/gentk-display-settings

---
+++ src/queries/objkt.ts
@@ -0,0 +1,146 @@
+import { gql } from "@apollo/client"
+import { Frag_GenAuthor } from "./fragments/generative-token"
+import { Frag_MediaImage } from "./fragments/media"
+
+export const Qu_objkt = gql`
+  ${Frag_GenAuthor}
+  ${Frag_MediaImage}
+  query Gentk($id: Float, $slug: String) {
+    objkt(id: $id, slug: $slug) {
+      id
+      version
+      royalties
+      royaltiesSplit {
+        pct
+        user {
+          id
+          name
+          flag
+          avatarUri
+        }
+      }
+      assigned
+      owner {
+        id
+        name
+        flag
+        avatarUri
+      }
+      name
+      slug
+      issuer {
+        id
+        name
+        flag
+        moderationReason
+        slug
+        labels
+        generativeUri
+        marketStats {
+          floor
+        }
+        ...Author
+      }
+      metadata
+      metadataUri
+      captureMedia {
+        ...MediaImage
+      }
+      features
+      rarity
+      assigned
+      duplicate
+      iteration
+      generationHash
+      createdAt
+      activeListing {
+        id
+        version
+        price
+        issuer {
+          id
+          name
+          flag
+          avatarUri
+        }
+      }
+      offers(filters: { active_eq: true }) {
+        id
+        price
+        version
+        createdAt
+        cancelledAt
+        acceptedAt
+        buyer {
+          id
+          name
+        }
+      }
+      actions {
+        id
+        opHash
+        type
+        numericValue
+        metadata
+        createdAt
+        issuer {
+          id
+          name
+          flag
+          avatarUri
+        }
+        target {
+          id
+          name
+          flag
+          avatarUri
+        }
+        objkt {
+          name
+          id
+          iteration
+        }
+      }
+    }
+  }
+`
+
+export const Qu_objktsFeed = gql`
+  ${Frag_GenAuthor}
+  query Query($filters: ObjktFilter, $take: Int) {
+    objkts(filters: $filters, take: $take) {
+      id
+      version
+      royalties
+      owner {
+        id
+        name
+        flag
+        avatarUri
+      }
+      name
+      slug
+      issuer {
+        id
+        name
+        slug
+        labels
+        generativeUri
+        ...Author
+      }
+      metadata
+      features
+      rarity
+      assigned
+      iteration
+      generationHash
+      createdAt
+      assignedAt
+      activeListing {
+        id
+        version
+        price
+      }
+    }
+  }
+`
