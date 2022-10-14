0bacaa1305d2b181d600a1ec1cc9cb5a58689f8e
maerzhase <markus@lili16.de>
2022-10-14 17:45:26 +0200

    Revert "add types for cardsizes"
    This reverts commit a6f0efe077b5062bf273187adc115ab8451d758e.

--- src/components/Input/CardSizeSelect.tsx
+++ src/components/Input/CardSizeSelect.tsx
@@ -1,15 +1,10 @@
 import React from "react"
 import cs from "classnames"
 import style from "./CardSizeSelect.module.scss"
-import {
-  CARDSIZE_SMALL,
-  CARDSIZE_MEDIUM,
-  CARDSIZE_LARGE,
-} from "../../types/Components/CardsContainer"
 
 interface ISizeOption {
   label: "small" | "medium" | "large"
-  value: CARDSIZE_SMALL | CARDSIZE_MEDIUM | CARDSIZE_LARGE
+  value: number
 }
 
 const defaultCardSizeOptions: ISizeOption[] = [
--- src/types/Components/CardsContainer.ts
+++ src/types/Components/CardsContainer.ts
@@ -1,12 +1,6 @@
 import { HTMLAttributes, PropsWithChildren } from "react"
 
-export type CARDSIZE_SMALL = 200
-export type CARDSIZE_MEDIUM = 270
-export type CARDSIZE_LARGE = 400
-
-export interface ICardContainerProps
-  extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
-  cardSize?: CARDSIZE_SMALL | CARDSIZE_MEDIUM | CARDSIZE_LARGE
+export interface ICardContainerProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+  cardSize?: number
   addDivs?: boolean
 }
-
