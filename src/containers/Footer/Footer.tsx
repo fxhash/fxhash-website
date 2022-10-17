e32bc9c3eedb78b6dc7c7445d321047317b5d525
maerzhase <markus@lili16.de>
2022-10-17 21:31:47 +0200

    add indexer status small to footer

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import style from "./Footer.module.scss"
 import cs from "classnames"
 import Link from "next/link"
 import { Logo } from "./Logo"
-
-
+import { IndexerStatusSmall } from "../../components/Status/IndexerStatusSmall"
+import { useFetchIndexerStati } from "../../hooks/useFetchIndexerStati"
 interface SocialProps {
   icon: string
   url: string
@@ -12,21 +12,33 @@ interface SocialProps {
 function FooterSocial({ icon, url }: SocialProps) {
   return (
     <Link href={url}>
-      <a><i aria-hidden className={`fab fa-${icon}`}/></a>
+      <a>
+        <i aria-hidden className={`fab fa-${icon}`} />
+      </a>
     </Link>
   )
 }
 
 export function Footer() {
+  const [indexerStatus, networkStatus] = useFetchIndexerStati()
   return (
     <footer className={cs(style.footer)}>
       <div className={cs(style.content)}>
         <div className={cs(style.logo_wrapper)}>
-          <Logo/>
+          <Logo />
           <div className={cs(style.socials)}>
-            <FooterSocial icon="twitter" url={process.env.NEXT_PUBLIC_URL_TWITTER!} />
-            <FooterSocial icon="instagram" url={process.env.NEXT_PUBLIC_URL_INSTAGRAM!} />
-            <FooterSocial icon="discord" url={process.env.NEXT_PUBLIC_URL_DISCORD!} />
+            <FooterSocial
+              icon="twitter"
+              url={process.env.NEXT_PUBLIC_URL_TWITTER!}
+            />
+            <FooterSocial
+              icon="instagram"
+              url={process.env.NEXT_PUBLIC_URL_INSTAGRAM!}
+            />
+            <FooterSocial
+              icon="discord"
+              url={process.env.NEXT_PUBLIC_URL_DISCORD!}
+            />
           </div>
         </div>
         <div>
@@ -46,15 +58,27 @@ export function Footer() {
             <Link href="https://feedback.fxhash.xyz">
               <a target="_blank">Feature request</a>
             </Link>
+            <Link href="/status">
+              <a>
+                <IndexerStatusSmall
+                  status={indexerStatus}
+                  networkStatus={networkStatus}
+                />
+              </a>
+            </Link>
           </div>
         </div>
       </div>
       <div className={cs(style.powered)}>
         <span>
           powered by{" "}
-          <a href="https://tzkt.io/" target="_blank">tzkt</a>{" "}
+          <a href="https://tzkt.io/" target="_blank" rel="noreferrer">
+            tzkt
+          </a>{" "}
           &amp;{" "}
-          <a href="https://smartpy.io/" target="_blank">SmartPy</a>
+          <a href="https://smartpy.io/" target="_blank" rel="noreferrer">
+            SmartPy
+          </a>
         </span>
       </div>
     </footer>
@@ -66,16 +90,33 @@ export function Footer() {
         <div className={cs(style.details)}>
           <span>and we were born to witness it</span>
           <div className={cs(style.socials)}>
-            <FooterSocial icon="twitter" url={process.env.NEXT_PUBLIC_URL_TWITTER!} />
-            <FooterSocial icon="instagram" url={process.env.NEXT_PUBLIC_URL_INSTAGRAM!} />
-            <FooterSocial icon="discord" url={process.env.NEXT_PUBLIC_URL_DISCORD!} />
+            <FooterSocial
+              icon="twitter"
+              url={process.env.NEXT_PUBLIC_URL_TWITTER!}
+            />
+            <FooterSocial
+              icon="instagram"
+              url={process.env.NEXT_PUBLIC_URL_INSTAGRAM!}
+            />
+            <FooterSocial
+              icon="discord"
+              url={process.env.NEXT_PUBLIC_URL_DISCORD!}
+            />
           </div>
         </div>
       </div>
       <div className={cs(style.powered)}>
-        <span><Link href="/fix-gentk"><a>🚑 FIX Gentk 🚑</a></Link></span>
-        <span>powered by <a href="https://tzkt.io/">TzTK API</a> &amp; <a href="https://smartpy.io/">SmartPy</a></span>
+        <span>
+          <Link href="/fix-gentk">
+            <a>🚑 FIX Gentk 🚑</a>
+          </Link>
+        </span>
+        <span>
+          powered by <a href="https://tzkt.io/">TzTK API</a> &amp;{" "}
+          <a href="https://smartpy.io/">SmartPy</a>
+        </span>
       </div>
     </footer>
   )
 }
+
