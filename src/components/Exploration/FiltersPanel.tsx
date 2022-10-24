79881ec415261f60a1c956124641905c3d4ef46e
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-10-24 21:44:54 +0100

    feat(responsive): add mobile animation to filters, enabling filters translate from bottom to top

--- src/components/Exploration/FiltersPanel.tsx
+++ src/components/Exploration/FiltersPanel.tsx
@@ -3,11 +3,20 @@ import cs from "classnames"
 import { PropsWithChildren } from "react"
 
 interface Props {
+  open?: boolean
   onClose?: () => void
 }
-export function FiltersPanel({ onClose, children }: PropsWithChildren<Props>) {
+export function FiltersPanel({
+  open = true,
+  onClose,
+  children,
+}: PropsWithChildren<Props>) {
   return (
-    <div className={cs(style.root)}>
+    <div
+      className={cs(style.root, {
+        [style.visible]: open,
+      })}
+    >
       <div className={cs(style.mobile_header)}>
         <h4>Filters</h4>
         <button type="button" onClick={onClose}>
