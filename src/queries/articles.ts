0f0d16bb9fc16a6e66347eaf76cd8055e5841680
maerzhase <markus@lili16.de>
2022-10-28 17:44:20 +0200

    add missing export

--- src/queries/articles.ts
+++ src/queries/articles.ts
@@ -1,16 +1,25 @@
-import { gql } from "@apollo/client";
-import { Frag_ArticleFull, Frag_ArticleInfos, Frag_ArticleInfosAction } from "./fragments/article";
-import { Frag_ListingArticle } from "./fragments/listing";
+import { gql } from "@apollo/client"
+import {
+  Frag_ArticleFull,
+  Frag_ArticleInfos,
+  Frag_ArticleInfosAction,
+} from "./fragments/article"
+import { Frag_ListingArticle } from "./fragments/listing"
 import { Frag_UserBadge } from "./fragments/user"
 
 export const Qu_articles = gql`
-  query Articles($filters: ArticleFilter, $sort: ArticleSortInput, $skip: Int, $take: Int) {
+  query Articles(
+    $filters: ArticleFilter
+    $sort: ArticleSortInput
+    $skip: Int
+    $take: Int
+  ) {
     articles(filters: $filters, sort: $sort, skip: $skip, take: $take) {
       ...ArticleInfos
     }
   }
   ${Frag_ArticleInfos}
-`;
+`
 export const Qu_articleBySlug = gql`
   query ArticleBySlug($slug: String!) {
     article(slug: $slug) {
@@ -18,7 +27,7 @@ export const Qu_articleBySlug = gql`
     }
   }
   ${Frag_ArticleFull}
-`;
+`
 export const Qu_articleById = gql`
   query ArticleById($id: Int!) {
     article(id: $id) {
@@ -26,7 +35,7 @@ export const Qu_articleById = gql`
     }
   }
   ${Frag_ArticleFull}
-`;
+`
 export const Qu_articleActionsById = gql`
   query ArticleActionsById($id: Int!) {
     article(id: $id) {
@@ -58,8 +67,8 @@ export const Qu_articleListingsById = gql`
   }
   ${Frag_ListingArticle}
 `
-const Qu_articleActions = gql`
-  query Query ($id: Int!, $skip: Int, $take: Int, $filters: ActionFilter) {
+export const Qu_articleActions = gql`
+  query Query($id: Int!, $skip: Int, $take: Int, $filters: ActionFilter) {
     article(id: $id) {
       id
       actions(skip: $skip, take: $take, filters: $filters) {
