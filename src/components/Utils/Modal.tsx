017b6ba4183532f39026a526be7de48687877947
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-11-03 16:22:16 +0000

    feat: opened modal block body scrollable

--- src/components/Utils/Modal.tsx
+++ src/components/Utils/Modal.tsx
@@ -1,8 +1,9 @@
 import style from "./Modal.module.scss"
 import cs from "classnames"
 import { Cover } from "./Cover"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useCallback, useEffect } from "react"
 import effects from "../../styles/Effects.module.scss"
+import ReactDOM from "react-dom"
 
 export interface Props {
   title: string
@@ -17,21 +18,29 @@ export function Modal({
   className,
   children,
 }: PropsWithChildren<Props>) {
-  return (
+  const handleClose = useCallback(() => {
+    document.body.classList.remove("modal-open")
+    onClose()
+  }, [onClose])
+  useEffect(() => {
+    document.body.classList.add("modal-open")
+  }, [])
+  return ReactDOM.createPortal(
     <>
-      <Cover onClick={onClose} index={index} />
+      <Cover onClick={handleClose} index={index} />
       <div
         className={cs(style.modal, effects["drop-shadow-big"], className)}
         style={{ zIndex: index + 1 }}
       >
         <header>
           <span>{title}</span>
-          <button className={cs(style.btn_close)} onClick={onClose}>
+          <button className={cs(style.btn_close)} onClick={handleClose}>
             <i aria-hidden className="far fa-times" />
           </button>
         </header>
         <main>{children}</main>
       </div>
-    </>
+    </>,
+    document.body
   )
 }
