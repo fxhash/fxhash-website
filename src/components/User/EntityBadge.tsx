89075f2786996aba54c0b219db55cbd33caea30c
maerzhase <markus@lili16.de>
2022-11-21 17:15:46 +0100

    fix #487

--- src/components/User/EntityBadge.tsx
+++ src/components/User/EntityBadge.tsx
@@ -3,7 +3,7 @@ import { CollabBadge } from "./CollabBadge"
 import { UserBadge } from "./UserBadge"
 
 export interface IProps {
-  user: User
+  user: User | undefined
   size?: "small" | "regular" | "big" | "xl"
   prependText?: string
   hasLink?: boolean
@@ -26,7 +26,7 @@ export interface IProps {
  */
 export function EntityBadge(props: IProps) {
   const { user } = props
-
+  if (!user) return null
   return user.type === UserType.COLLAB_CONTRACT_V1 ? (
     <CollabBadge {...props} user={props.user as Collaboration} />
   ) : (
