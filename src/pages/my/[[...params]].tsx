dac699380919304fda80deccfe3ad54c2ae9bbcf
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2022-12-13 17:51:01 +0100

    feat: add url /my/... that redirect on profile and their subroutes

--- src/pages/my/[[...params]].tsx
+++ src/pages/my/[[...params]].tsx
@@ -1,19 +1,39 @@
-import React, { useContext } from "react"
-import { NextPage } from "next"
+import React, { useContext, useEffect } from "react"
 import Head from "next/head"
-import { UserContext } from "../../containers/UserProvider";
+import { UserContext } from "../../containers/UserProvider"
+import { LoaderBlock } from "../../components/Layout/LoaderBlock"
+import { useRouter } from "next/router"
+import { Spacing } from "../../components/Layout/Spacing"
+import cs from "classnames"
+import layout from "../../styles/Layout.module.scss"
 
-const Me: NextPage = () => {
-  const { user, userFetched } = useContext(UserContext)
+export default function My() {
+  const router = useRouter()
+  const { user, userFetched, autoConnectChecked } = useContext(UserContext)
+
+  useEffect(() => {
+    if (user && userFetched) {
+      const myReplacement = user.name ? `u/${user.name}` : `pkh/${user.id}`
+      const path = `/${myReplacement}${router.asPath.slice(3)}`
+      router.replace(path, path)
+      return
+    }
+  }, [router, user, userFetched])
   return (
     <>
       <Head>
-        <title>fxhash — me</title>
-        <meta key="og:title" property="og:title" content="fxhash — me" />
+        <title>fxhash — my</title>
+        <meta key="og:title" property="og:title" content="fxhash — my" />
       </Head>
-      <section>
-        loading
-      </section>
+      <main className={cs(layout["padding-big"], layout.y_centered)}>
+        <Spacing size="3x-large" />
+        {user && <LoaderBlock />}
+        {!user && !userFetched && autoConnectChecked && (
+          <p className={layout.text_centered}>
+            Connect your wallet to access your profile
+          </p>
+        )}
+      </main>
     </>
   )
 }
