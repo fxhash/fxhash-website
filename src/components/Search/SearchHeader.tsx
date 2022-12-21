1415d7f1d3795c7341dfe44a43b77a3d860cb172
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2022-12-21 20:26:11 +0100

    ≡ƒöº allow winter url prod

---
+++ src/components/Search/SearchHeader.tsx
@@ -0,0 +1,64 @@
+import style from "./Search.module.scss"
+import layout from "../../styles/Layout.module.scss"
+import cs from "classnames"
+import { forwardRef, PropsWithChildren } from "react"
+
+interface Props {
+  sortSelectComp?: React.ReactNode
+  sizeSelectComp?: React.ReactNode
+  hasFilters?: boolean
+  onToggleFilters?: () => void
+  filtersOpened?: boolean
+  padding?: "big" | "small"
+  showFiltersOnMobile?: boolean
+}
+export const SearchHeader = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<Props>
+>(
+  (
+    {
+      sortSelectComp,
+      sizeSelectComp,
+      hasFilters,
+      onToggleFilters,
+      filtersOpened,
+      padding = "big",
+      children,
+      showFiltersOnMobile = true,
+    },
+    ref
+  ) => {
+    return (
+      <div
+        className={cs(style.search_header, layout[`padding-${padding}`])}
+        ref={ref}
+      >
+        <div className={cs(style.search_wrapper)}>
+          {hasFilters && (
+            <button
+              type="button"
+              className={cs(style.filter_btn, {
+                [style.filters_opened]: filtersOpened,
+                [style["filter_btn--show-mobile"]]: showFiltersOnMobile,
+              })}
+              onClick={onToggleFilters}
+            >
+              {filtersOpened ? (
+                <i aria-hidden className="fas fa-caret-left" />
+              ) : (
+                <i aria-hidden className="fas fa-filter" />
+              )}
+              <span>filters</span>
+            </button>
+          )}
+          {children}
+        </div>
+        {sizeSelectComp}
+        {sortSelectComp}
+      </div>
+    )
+  }
+)
+
+SearchHeader.displayName = "SearchHeader"
---
+++ src/components/Utils/Modal.tsx
@@ -0,0 +1,49 @@
+import style from "./Modal.module.scss"
+import cs from "classnames"
+import { Cover } from "./Cover"
+import { PropsWithChildren, useCallback, useEffect } from "react"
+import effects from "../../styles/Effects.module.scss"
+import ReactDOM from "react-dom"
+
+export interface Props {
+  title: string
+  onClose: () => void
+  index?: number
+  className?: string
+}
+export function Modal({
+  title,
+  index = 999999,
+  onClose,
+  className,
+  children,
+}: PropsWithChildren<Props>) {
+  const handleClose = useCallback(() => {
+    document.body.classList.remove("modal-open")
+    onClose()
+  }, [onClose])
+  useEffect(() => {
+    document.body.classList.add("modal-open")
+    return () => {
+      document.body.classList.remove("modal-open")
+    }
+  }, [])
+  return ReactDOM.createPortal(
+    <>
+      <Cover onClick={handleClose} index={index} />
+      <div
+        className={cs(style.modal, effects["drop-shadow-big"], className)}
+        style={{ zIndex: index + 1 }}
+      >
+        <header>
+          <span>{title}</span>
+          <button className={cs(style.btn_close)} onClick={handleClose}>
+            <i aria-hidden className="far fa-times" />
+          </button>
+        </header>
+        <main>{children}</main>
+      </div>
+    </>,
+    document.body
+  )
+}
