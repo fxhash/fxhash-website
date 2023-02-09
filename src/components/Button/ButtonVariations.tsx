c7ae9e750ebd395bc703a3a971e6278214ba79d0
maerzhase <markus@lili16.de>
2023-02-09 15:30:56 +0100

    move TezQuakeAid first in list

---
+++ src/components/Button/ButtonVariations.tsx
@@ -0,0 +1,112 @@
+import style from "./ButtonVariations.module.scss"
+import cs from "classnames"
+import { GenerativeToken } from "../../types/entities/GenerativeToken"
+import { Button } from "."
+import { useContext, useMemo } from "react"
+import { generateFxHash } from "../../utils/hash"
+import { uniq } from "lodash"
+import { HoverTitle } from "../Utils/HoverTitle"
+import { SettingsContext } from "../../context/Theme"
+
+interface Props {
+  token: Pick<GenerativeToken, "metadata" | "balance">
+  previewHash?: string | null
+  onChangeHash: (hash: string) => void
+}
+export function ButtonVariations({ token, previewHash, onChangeHash }: Props) {
+  // some shortcuts to get access to useful variables derived from the token data
+  const fullyMinted = token.balance === 0
+  const exploreSet = token.metadata.settings?.exploration
+
+  // settings activated based on fully minted state
+  const activeSettings = fullyMinted
+    ? exploreSet?.postMint
+    : exploreSet?.preMint
+
+  // the hover message is only here if disabled, and depends some conditions
+  const hoverMessage = useMemo<string | null>(() => {
+    if (!token.metadata.settings?.exploration) {
+      return "This token doesn't have variation settings, therefore exploration of variations is disabled"
+    }
+    if (!activeSettings?.enabled) {
+      if (fullyMinted) {
+        return "Artist disabled the exploration of variations after minting phase"
+      } else {
+        return "Artist disabled the exploration of variation during the minting phase"
+      }
+    }
+    return null
+  }, [])
+
+  // a list of hashes to explore, if enabled
+  const hashes = useMemo<string[] | null>(() => {
+    if (!activeSettings?.hashConstraints) {
+      return null
+    }
+    return uniq([
+      token.metadata.previewHash!,
+      ...activeSettings.hashConstraints,
+    ])
+  }, [])
+
+  // the component rendering the icon next to the button
+  const icon = useMemo(() => {
+    if (activeSettings?.enabled) {
+      // if there is an infinite number of variations
+      if (!activeSettings?.hashConstraints) {
+        return <i aria-hidden className="fas fa-infinity" />
+      }
+      // otherwise there is a finite amount of variations, we need to display the active
+      else {
+        // find index of the active hash
+        let idx = hashes?.indexOf(previewHash!)
+        idx = idx === -1 || idx == null ? 0 : idx
+        return (
+          <div className={cs(style.progress)}>
+            {idx + 1}/{hashes!.length}
+          </div>
+        )
+      }
+    } else {
+      return <i aria-hidden className="fas fa-infinity" />
+    }
+  }, [previewHash])
+
+  // update the preview hash, either by looping through available hashes, or by
+  // generating a random hash
+  const updatePreviewHash = () => {
+    // small front security for those removing the disabled property on the buttons
+    if (!activeSettings?.enabled) {
+      return
+    }
+    // if no hash constraints, just give a random hash
+    if (!hashes) {
+      onChangeHash(generateFxHash())
+    }
+    // if there is a list of hashes, cycle through those
+    else {
+      // find index of the active hash
+      let idx = hashes?.indexOf(previewHash!)
+      idx = idx === -1 || idx == null ? 0 : idx
+      // compute the new index
+      idx = (idx + 1) % hashes.length
+      onChangeHash(hashes[idx])
+    }
+  }
+
+  return (
+    <HoverTitle message={hoverMessage}>
+      <Button
+        type="button"
+        size="small"
+        color="transparent"
+        disabled={!activeSettings?.enabled}
+        iconComp={icon}
+        iconSide="right"
+        onClick={updatePreviewHash}
+      >
+        variations
+      </Button>
+    </HoverTitle>
+  )
+}
---
+++ src/components/CreditCard/WinterCheckout.tsx
@@ -0,0 +1,124 @@
+import React, {
+  FunctionComponent,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react"
+import style from "./WinterCheckout.module.scss"
+
+type TSuccess = (transactionHash: string, amountUSD: number) => void
+
+interface Props {
+  projectId?: number
+  showModal?: boolean
+  contractAddress?: string
+  contractVersion?: number
+  tokenId?: string
+  walletAddress?: string
+  email?: string
+  title?: string
+  brandImage?: string
+  mintQuantity?: number
+  extraMintParams?: object
+  priceFunctionParams?: object
+  production?: boolean
+  fillSource?: string
+  orderSource?: string
+  language?: string
+  gentkId?: number
+  listingId?: number
+  assetId?: string
+  appearance?: Record<string, string | number | undefined>
+  onClose?: () => void
+  onSuccess?: TSuccess
+  onFinish: (data: any) => void
+}
+
+const WinterCheckout: FunctionComponent<Props> = (props) => {
+  const {
+    onSuccess,
+    onClose,
+    projectId,
+    showModal,
+    production,
+    contractAddress,
+    tokenId,
+    onFinish,
+  } = props
+  const [projectUrl, setProjectUrl] = useState("")
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleWindowEvent = (e: MessageEvent) => {
+        const { data } = e
+        if (data === "closeWinterCheckoutModal") {
+          onClose?.()
+        } else if (data.reveal || data.viewNFTs) {
+          onFinish(data)
+        } else if (data.name === "successfulWinterCheckout") {
+          const { transactionHash, amountUSD } = data
+          onSuccess?.(transactionHash, amountUSD)
+        }
+      }
+      window.addEventListener("message", handleWindowEvent)
+      return () => window.removeEventListener("message", handleWindowEvent)
+    }
+  }, [onClose, onSuccess, onFinish])
+
+  useEffect(() => {
+    let queryParams = []
+    if (projectId) {
+      queryParams.push(`projectId=${projectId}`)
+    } else if (contractAddress && tokenId) {
+      queryParams.push(
+        `contractAddress=${contractAddress}`,
+        `tokenId=${tokenId}`
+      )
+    }
+
+    const paramsStr = [
+      "contractVersion",
+      "walletAddress",
+      "email",
+      "mintQuantity",
+      "fillSource",
+      "orderSource",
+      "title",
+      "language",
+      "brandImage",
+      "gentkId",
+      "listingId",
+      "assetId",
+    ]
+    paramsStr.forEach((param) => {
+      const propValue = props[param as keyof PropsWithChildren<Props>]
+      if (propValue) {
+        queryParams.push(`${param}=${propValue}`)
+      }
+    })
+
+    const paramsObj = ["extraMintParams", "priceFunctionParams", "appearance"]
+    paramsObj.forEach((param) => {
+      const propValue = props[param as keyof PropsWithChildren<Props>]
+      if (propValue) {
+        queryParams.push(
+          `${param}=${encodeURIComponent(JSON.stringify(propValue))}`
+        )
+      }
+    })
+
+    const queryString = queryParams.join("&")
+    const url = production
+      ? "https://checkout.usewinter.com/?" + queryString
+      : "https://sandbox-winter-checkout.onrender.com/?" + queryString
+    setProjectUrl(url)
+  }, [contractAddress, production, projectId, props, tokenId])
+
+  return showModal ? (
+    <iframe id="winter-checkout" src={projectUrl} className={style.iframe} />
+  ) : (
+    <></>
+  )
+}
+
+export default WinterCheckout
