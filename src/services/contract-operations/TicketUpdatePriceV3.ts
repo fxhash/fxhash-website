238a0fcb8ecdfaf2b546fce3f1bf56834ad0c453
ciphrd <crespy.b@gmail.com>
2023-03-07 15:40:21 +0100

    added contract operations for tickets [TODO: set proper entity type for tickets]

---
+++ src/services/contract-operations/TicketUpdatePriceV3.ts
@@ -0,0 +1,46 @@
+import {
+  ContractAbstraction,
+  TransactionWalletOperation,
+  Wallet,
+} from "@taquito/taquito"
+import { NFTArticle } from "../../types/entities/Article"
+import { FxhashContracts } from "../../types/Contracts"
+import { ContractOperation } from "./ContractOperation"
+
+export interface ITaxationSettings {
+  coverage: number
+  price: number
+}
+
+export type TTicketUpdatePriceV3OperationParams = {
+  ticket: any
+  amount: number
+  taxationSettings: ITaxationSettings
+}
+
+/**
+ * Mint an unique iteration of a Generative Token
+ */
+export class TicketUpdatePriceV3Operation extends ContractOperation<TTicketUpdatePriceV3OperationParams> {
+  contract: ContractAbstraction<Wallet> | null = null
+
+  async prepare() {
+    this.contract = await this.manager.getContract(
+      FxhashContracts.MINT_TICKETS_V3
+    )
+  }
+
+  async call(): Promise<TransactionWalletOperation> {
+    return this.contract!.methodsObject.update_price({
+      token_id: this.params.ticket.id,
+      taxation: this.params.taxationSettings,
+    }).send({
+      amount: this.params.amount,
+      mutez: true,
+    })
+  }
+
+  success(): string {
+    return `You have successfully updated the ticket ${this.params.ticket} pricing settings.`
+  }
+}
