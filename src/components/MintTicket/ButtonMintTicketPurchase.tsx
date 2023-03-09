c4ac0a1708600dcfebb7a4e162e7319a6a112711
ciphrd <crespy.b@gmail.com>
2023-03-09 13:46:33 +0100

    flow from "mint ticket" to explore page to mint

--- src/components/MintTicket/ButtonMintTicketPurchase.tsx
+++ src/components/MintTicket/ButtonMintTicketPurchase.tsx
@@ -1,22 +1,28 @@
 import React, { memo, useCallback, useState } from "react"
+import cs from "classnames"
 import { Button } from "../Button"
 import { MintTicket } from "../../types/entities/MintTicket"
 import { DisplayTezos } from "../Display/DisplayTezos"
 import { Modal } from "../Utils/Modal"
 import style from "./MintTicketModal.module.scss"
 import text from "../../styles/Text.module.css"
-import { Spacing } from "../Layout/Spacing";
+import { Spacing } from "../Layout/Spacing"
+import Link from "next/link"
+import { getGenerativeTokenUrl } from "utils/generative-token"
+import { GenerativeToken } from "types/entities/GenerativeToken"
 
 interface ButtonMintTicketPurchaseProps {
-  tokenName: string
+  token: GenerativeToken
   gracingPeriod: number
   price: number
+  ticketId: number | null
 }
 
 const _ButtonMintTicketPurchase = ({
-  tokenName,
+  token,
   gracingPeriod,
   price,
+  ticketId,
 }: ButtonMintTicketPurchaseProps) => {
   const [showModal, setShowModal] = useState(false)
   const handleToggleModal = useCallback(
@@ -28,14 +34,17 @@ const _ButtonMintTicketPurchase = ({
       <Button
         type="button"
         color="secondary"
-        size="small"
+        size="regular"
+        iconComp={<i aria-hidden className="fas fa-arrow-right" />}
+        iconSide="right"
         onClick={handleToggleModal(true)}
       >
-        mint ticket
+        see ticket
       </Button>
+
       {showModal && (
         <Modal
-          title={`You have purchased a mint ticket for “${tokenName}”`}
+          title={`You have purchased a mint ticket for “${token.name}”`}
           onClose={handleToggleModal(false)}
         >
           <p className={style.p}>
@@ -49,7 +58,9 @@ const _ButtonMintTicketPurchase = ({
             potenti maecenas aliquet consectetur duis. In arcu convallis vel
             feugiat dolor varius risus.
           </p>
-          <Spacing size="regular" />
+
+          <Spacing size="large" />
+
           <div className={style.container_inputs}>
             <div className={style.container_input}>
               <label>
@@ -59,7 +70,7 @@ const _ButtonMintTicketPurchase = ({
                   don’t pay a tax
                 </div>
               </label>
-              <div className={text.bold}>{gracingPeriod} days</div>
+              <div className={cs(text.bold, text.h4)}>{gracingPeriod} days</div>
             </div>
             <div className={style.container_input}>
               <label>
@@ -69,7 +80,7 @@ const _ButtonMintTicketPurchase = ({
                   want to pay as a tax
                 </div>
               </label>
-              <div className={text.bold}>
+              <div className={cs(text.bold, text.h4)}>
                 <DisplayTezos
                   mutez={price}
                   formatBig={false}
@@ -78,13 +89,23 @@ const _ButtonMintTicketPurchase = ({
               </div>
             </div>
           </div>
+
+          <Spacing size="x-large" />
+
           <div className={style.container_buttons}>
             <Button type="button" size="small">
               update the price
             </Button>
-            <Button type="button" color="secondary" size="small">
-              mint your iteration
-            </Button>
+            {ticketId !== null && (
+              <Link
+                href={`${getGenerativeTokenUrl(token)}/ticket/${ticketId}/mint`}
+                passHref
+              >
+                <Button type="button" color="secondary" size="small" isLink>
+                  mint your iteration
+                </Button>
+              </Link>
+            )}
           </div>
         </Modal>
       )}
