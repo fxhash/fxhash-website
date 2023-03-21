031a4d3a5e4ee8c792a0468dc00e1179f6a88155
maerzhase <markus@lili16.de>
2023-03-21 20:50:46 +0100

    add creator, owner and minter to meta table on gentk page

---
+++ src/components/Card/CardSmallNFTArticle.tsx
@@ -0,0 +1,47 @@
+import React, { memo, useContext, useMemo } from "react"
+import style from "./CardSmallNFTArticle.module.scss"
+import { NFTArticle } from "../../types/entities/Article"
+import { Image } from "../Image"
+import { ipfsGatewayUrl } from "../../services/Ipfs"
+import { SettingsContext } from "../../context/Theme"
+import cs from "classnames"
+import { UserBadge } from "../User/UserBadge"
+import Link from "next/link"
+import { getArticleUrl } from "../../utils/entities/articles"
+
+interface CardSmallNftArticleProps {
+  article: NFTArticle
+  imagePriority?: boolean
+  className?: string
+}
+
+const _CardSmallNftArticle = ({
+  article,
+  imagePriority,
+  className,
+}: CardSmallNftArticleProps) => {
+  const settings = useContext(SettingsContext)
+  const { title, description, thumbnailUri, thumbnailMedia, author } = article
+  return (
+    <div
+      className={cs(style.container, className, {
+        [style.hover_effect]: settings.hoverEffectCard,
+      })}
+    >
+      <div className={style["img-wrapper"]}>
+        <Image alt="" ipfsUri={thumbnailUri} image={thumbnailMedia} />
+      </div>
+      <div className={style.infos}>
+        <UserBadge user={author!} size="regular" />
+        <Link href={getArticleUrl(article)}>
+          <a className={style.title}>
+            <h5>{title}</h5>
+          </a>
+        </Link>
+        <p className={style.description}>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export const CardSmallNftArticle = memo(_CardSmallNftArticle)
---
+++ src/components/FxParams/ParamsHistory.tsx
@@ -0,0 +1,123 @@
+import {
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+} from "react"
+import { createContext } from "react"
+import debounce from "lodash.debounce"
+import { stringifyParamsData } from "./utils"
+
+const isEqual = (a: any, b: any) =>
+  stringifyParamsData(a) === stringifyParamsData(b)
+
+type ParamsHistoryActionType = "params-update" | "hash-update"
+
+export interface IParamsHistoryEntry {
+  type: ParamsHistoryActionType
+  oldValue: any
+  newValue: any
+}
+
+type ParamsHistoryAction = (entry: IParamsHistoryEntry) => void
+
+export interface IParamsHistoryContext {
+  registerAction: (t: ParamsHistoryActionType, a: ParamsHistoryAction) => void
+  history: IParamsHistoryEntry[]
+  pushHistory: (entry: IParamsHistoryEntry) => void
+  offset: number
+  setOffset: (o: number) => void
+  undo: () => void
+  redo: () => void
+}
+
+const defaultParamsHistoryContext: IParamsHistoryContext = {
+  registerAction: () => {},
+  history: [],
+  pushHistory: () => {},
+  offset: 0,
+  setOffset: () => {},
+  undo: () => {},
+  redo: () => {},
+}
+
+export const ParamsHistoryContext = createContext(defaultParamsHistoryContext)
+
+type ParamHistoryActions = Record<
+  ParamsHistoryActionType,
+  ParamsHistoryAction | undefined
+>
+
+type Props = PropsWithChildren<{}>
+
+export function ParamsHistoryProvider({ children }: Props) {
+  const [history, setHistory] = useState<IParamsHistoryEntry[]>([])
+  const [offset, setOffset] = useState<number>(-1)
+  const actions = useRef<Partial<ParamHistoryActions>>()
+
+  const pushHistory = (entry: IParamsHistoryEntry) => {
+    setHistory((prev) => [entry, ...prev])
+
+    const historyEntry = history[0]
+    const lastStatesPerActionType = Object.keys(actions?.current || {})
+      .filter((actionType) => actionType !== entry.type)
+      .map((actionType) => {
+        return history.find((historyEntry) => historyEntry.type === actionType)
+      })
+    lastStatesPerActionType.forEach((historyEntry) => {
+      if (!historyEntry) return
+      actions?.current?.[historyEntry?.type]?.(historyEntry?.newValue)
+    })
+    setOffset(-1)
+  }
+
+  const pushHistoryDebounced = useCallback(debounce(pushHistory, 200), [
+    history,
+    setHistory,
+    setOffset,
+  ])
+
+  const undo = () => {
+    if (offset >= history.length) return
+    const nextOffset = offset + 1
+    setOffset(nextOffset)
+    const historyEntry = history[nextOffset]
+    actions?.current?.[historyEntry.type]?.(historyEntry.oldValue)
+  }
+
+  const redo = () => {
+    if (offset <= -1) return
+    const historyEntry = history[offset]
+    actions?.current?.[historyEntry.type]?.(historyEntry.newValue)
+    const prevOffset = offset - 1
+    setOffset(prevOffset)
+  }
+
+  const registerAction = (
+    actionType: ParamsHistoryActionType,
+    action: ParamsHistoryAction
+  ) => {
+    actions.current = {
+      ...actions?.current,
+      [actionType]: action,
+    }
+  }
+
+  const context: IParamsHistoryContext = {
+    registerAction,
+    history,
+    pushHistory: pushHistoryDebounced,
+    offset,
+    setOffset,
+    undo,
+    redo,
+  }
+
+  return (
+    <ParamsHistoryContext.Provider value={context}>
+      {children}
+    </ParamsHistoryContext.Provider>
+  )
+}
