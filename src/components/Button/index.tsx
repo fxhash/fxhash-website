634f8db0651e47a66d5fb3e9bdd9add46f1cd69e
louis <louisholley@me.com>
2023-03-23 21:12:23 +0000

    add button size full

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -14,6 +14,7 @@ export type ButtonSize =
   | "very-small"
   | "very-large"
   | "large-x"
+  | "full"
   | "custom"
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
