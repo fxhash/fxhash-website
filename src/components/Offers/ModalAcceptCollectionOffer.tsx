948dca271bf418fda3c824faab4914ee7468b17d
louis <louisholley@me.com>
2023-03-30 15:15:06 +0100

    add CollectionOfferActions with modal for accept

---
+++ src/components/Offers/ModalAcceptCollectionOffer.tsx
@@ -0,0 +1,84 @@
+import React, { memo, useContext, useState } from "react"
+import { Button } from "../Button"
+import { Modal } from "../Utils/Modal"
+import style from "./ModalAcceptCollectionOffer.module.scss"
+import { Spacing } from "../Layout/Spacing"
+import { CollectionOffer } from "types/entities/Offer"
+import { Objkt } from "types/entities/Objkt"
+import { useQuery } from "@apollo/client"
+import { UserContext } from "containers/UserProvider"
+import { Qu_userAcceptCollectionOffer } from "queries/user"
+import { Image } from "components/Image"
+import { Checkbox } from "components/Input/Checkbox"
+
+interface ModalAcceptCollectionOfferProps {
+  offer: CollectionOffer
+  onClose: () => void
+  onClickAccept: (selectedGentk: Objkt) => void
+}
+
+const _ModalAcceptCollectionOffer = ({
+  offer,
+  onClose,
+  onClickAccept,
+}: ModalAcceptCollectionOfferProps) => {
+  const { user } = useContext(UserContext)
+  const [selectedGentk, setSelectedGentk] = useState<Objkt | null>(null)
+
+  const { data, loading } = useQuery(Qu_userAcceptCollectionOffer, {
+    variables: {
+      userId: user?.id,
+      issuerId: offer.token.id,
+    },
+    fetchPolicy: "no-cache",
+  })
+
+  return (
+    <Modal
+      title={`Accept collection offer for ${offer.token.name}`}
+      onClose={onClose}
+    >
+      <p className={style.p}>Select which gentk you would like to sell</p>
+      <Spacing size="regular" />
+      <div className={style.gentks}>
+        {data?.user.objkts.map((gentk: Objkt) => (
+          <div key={gentk.id} className={style.gentk_container}>
+            <Checkbox
+              value={gentk.id === selectedGentk?.id}
+              onChange={(value) => setSelectedGentk(value ? gentk : null)}
+            />
+            <div
+              className={style.gentk}
+              onClick={() => setSelectedGentk(gentk)}
+            >
+              <div className={style.thumbnail_container}>
+                <Image
+                  ipfsUri={gentk.metadata?.thumbnailUri}
+                  image={gentk.captureMedia}
+                  alt={`thumbnail of ${gentk.name}`}
+                />
+              </div>
+              <span className={style.container_name}>
+                <span className={style.name}>{gentk.name}</span>
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Spacing size="large" />
+      <div className={style.container_buttons}>
+        <Button
+          color="secondary"
+          size="small"
+          state={loading ? "loading" : "default"}
+          disabled={!selectedGentk}
+          onClick={() => onClickAccept(selectedGentk!)}
+        >
+          accept offer
+        </Button>
+      </div>
+    </Modal>
+  )
+}
+
+export const ModalAcceptCollectionOffer = memo(_ModalAcceptCollectionOffer)
