691177042d72b9f74f7d7bcfe340937c4e3e3313
louis <louisholley@me.com>
2023-03-30 15:12:37 +0100

    add CollectionOffer type, AnyOffer union

--- src/types/entities/Offer.ts
+++ src/types/entities/Offer.ts
@@ -1,3 +1,4 @@
+import { GenerativeToken } from "./GenerativeToken"
 import { Objkt } from "./Objkt"
 import { User } from "./User"
 
@@ -10,6 +11,24 @@ export interface Offer {
   createdAt: string
   cancelledAt: string
   acceptedAt: string
+}
+
+export interface CollectionOffer {
+  id: number
+  version: number
+  buyer: User
+  token: GenerativeToken
+  price: number
+  amount: number
+  createdAt: string
+  cancelledAt: string
+  completedAt: string
+}
+
+export type AnyOffer = Offer | CollectionOffer
+
+export const offerTypeGuard = (offer: AnyOffer): offer is Offer => {
+  return (offer as Offer).objkt !== undefined
 }
 
 // export interface OfferFilters {
