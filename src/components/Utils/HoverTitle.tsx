324e32a7158e5bb904c81bbf2e0503e419ee736c
maerzhase <markus@lili16.de>
2023-04-16 15:24:23 +0200

    prevent tooltip out of window bounds

--- src/components/Utils/HoverTitle.tsx
+++ src/components/Utils/HoverTitle.tsx
@@ -1,6 +1,6 @@
 import style from "./HoverTitle.module.scss"
 import cs from "classnames"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, useEffect, useMemo, useRef, useState } from "react"
 
 interface Props {
   className?: string
@@ -11,14 +11,49 @@ export function HoverTitle({
   className,
   children,
 }: PropsWithChildren<Props>) {
+  const hoverMessageRef = useRef<HTMLDivElement>(null)
+  const anchorRef = useRef<HTMLDivElement>(null)
+  const [offsetX, setOffetX] = useState<number>(0)
+
+  useEffect(() => {
+    function calcScreenOverflow() {
+      if (!hoverMessageRef.current || !anchorRef.current) return
+      const { left, width: anchorWidth } =
+        anchorRef.current.getBoundingClientRect()
+      const { width, x } = hoverMessageRef.current.getBoundingClientRect()
+      const margin = 8;
+      if (window.outerWidth > left + width / 2) {
+        setOffetX(0)
+      } else {
+        setOffetX(
+          margin + width / 2 - (window.outerWidth - (left + anchorWidth / 2))
+        )
+      }
+    }
+    calcScreenOverflow()
+    window.addEventListener("resize", calcScreenOverflow)
+    return () => {
+      window.removeEventListener("resize", calcScreenOverflow)
+    }
+  }, [hoverMessageRef, anchorRef, setOffetX])
+
   return (
     <div
+      ref={anchorRef}
       className={cs(style.wrapper, className, {
         [style.hover_enabled]: !!message,
       })}
     >
       {children}
-      {message && <div className={cs(style.hover_message)}>{message}</div>}
+      {message && (
+        <div
+          ref={hoverMessageRef}
+          className={cs(style.hover_message)}
+          style={{ transform: `translate(calc(-50% - ${offsetX}px),0px)` }}
+        >
+          {message}
+        </div>
+      )}
     </div>
   )
 }
