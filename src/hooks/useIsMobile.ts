bca8ab04364fbdbb93dc27ef0a23837fce052744
maerzhase <markus@lili16.de>
2023-04-18 14:33:22 +0200

    run eslint:fix

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -4,5 +4,8 @@ import useWindowSize, { breakpoints } from "./useWindowsSize"
 export default function useIsMobile(breakpoint: number = breakpoints.md) {
   const { width } = useWindowSize()
 
-  return useMemo(() => width !== undefined && width <= breakpoint, [width, breakpoint])
+  return useMemo(
+    () => width !== undefined && width <= breakpoint,
+    [width, breakpoint]
+  )
 }
