5bee202c865627815ca7513ce22bbe020921794b
maerzhase <markus@lili16.de>
2023-04-18 14:30:09 +0200

    set default breakpoint for useIsMobile hook to md

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,8 +1,8 @@
 import { useMemo } from "react"
 import useWindowSize, { breakpoints } from "./useWindowsSize"
 
-export default function useIsMobile(breakpoint: number = breakpoints.sm) {
+export default function useIsMobile(breakpoint: number = breakpoints.md) {
   const { width } = useWindowSize()
 
-  return useMemo(() => width !== undefined && width <= breakpoint, [width])
+  return useMemo(() => width !== undefined && width <= breakpoint, [width, breakpoint])
 }
