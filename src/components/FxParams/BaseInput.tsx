53761c85a2d98e0dc5681c3322ed2de9be633e4c
Geoffrey MERRAN <geoffrey.merran@gmail.com>
2023-04-19 18:45:22 +0100

    feat: add incoming events to homepage

---
+++ src/components/FxParams/BaseInput.tsx
@@ -0,0 +1,39 @@
+import {
+  ButtonHTMLAttributes,
+  HtmlHTMLAttributes,
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+} from "react"
+import classes from "./BaseInput.module.scss"
+import cx from "classnames"
+
+export function BaseInput(props: InputHTMLAttributes<HTMLInputElement>) {
+  const { className, ...rest } = props
+  return <input className={cx(classes.baseInput, className)} {...rest} />
+}
+
+export function BaseSelect(props: SelectHTMLAttributes<HTMLSelectElement>) {
+  const { className, ...rest } = props
+  return <select className={cx(classes.baseSelect, className)} {...rest} />
+}
+
+export interface BaseButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: "primary" | "secondary" | "primary-inverted" | "main" | undefined
+}
+
+export function BaseButton(props: BaseButtonProps) {
+  const { className, color = "primary", ...rest } = props
+  return (
+    <button
+      type="button"
+      className={cx(classes.baseButton, classes[`color-${color}`], className)}
+      {...rest}
+    />
+  )
+}
+
+export function IconButton(props: BaseButtonProps) {
+  const { className, ...rest } = props
+  return <BaseButton className={cx(classes.iconButton, className)} {...rest} />
+}
---
+++ src/types/entities/Objkt.ts
@@ -0,0 +1,61 @@
+import { ObjktMetadata, TokenFeature } from "../Metadata"
+import { Action } from "./Action"
+import { GenerativeToken } from "./GenerativeToken"
+import { Listing } from "./Listing"
+import { Offer } from "./Offer"
+import { Split } from "./Split"
+import { User } from "./User"
+import { MediaImage } from "./MediaImage"
+
+export interface Objkt {
+  id: string
+  version: 0 | 1
+  issuer: GenerativeToken
+  owner?: User | null
+  minter?: User | null
+  assigned?: boolean
+  generationHash?: string
+  inputBytes?: string | null
+  duplicate?: boolean
+  iteration?: number
+  mintedPrice?: number
+  tags: string[]
+  name?: string
+  slug?: string
+  metadata?: ObjktMetadata
+  features?: TokenFeature[] | null
+  rarity?: number | null
+  metadataUri: string
+  royalties: number
+  royaltiesSplit: Split[]
+  activeListing?: Listing | null
+  offers?: Offer[]
+  actions: Action[]
+  createdAt: string
+  updatedAt: string
+  assignedAt: string | null
+  captureMedia?: MediaImage
+}
+
+export interface ObjktFilters {
+  activeListing_exist?: boolean
+}
+
+export enum EObjktFeatureType {
+  BOOLEAN = "BOOLEAN",
+  STRING = "STRING",
+  NUMBER = "NUMBER",
+}
+
+export interface IObjktFeatureFilter {
+  name: string
+  values: string[]
+  type: EObjktFeatureType
+}
+
+export function objktFeatureType(value: any): EObjktFeatureType {
+  const type = typeof value
+  if (type === "boolean") return EObjktFeatureType.BOOLEAN
+  else if (type === "number") return EObjktFeatureType.NUMBER
+  else return EObjktFeatureType.STRING
+}
