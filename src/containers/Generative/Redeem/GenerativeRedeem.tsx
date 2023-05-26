d24be7caf7f83f75f8583f01e388d64adeff2a94
louis <louisholley@me.com>
2023-05-26 12:30:03 +0100

    add GentkList to GenerativeRedeem

--- src/containers/Generative/Redeem/GenerativeRedeem.tsx
+++ src/containers/Generative/Redeem/GenerativeRedeem.tsx
@@ -10,7 +10,18 @@ import { RedeemableDetailsView } from "../../../components/Entities/RedeemableDe
 import { RedeemableDetails } from "../../../types/entities/Redeemable"
 import { GenerativeToken } from "../../../types/entities/GenerativeToken"
 import style from "./GenerativeRedeem.module.scss"
+import { CardsExplorer } from "components/Exploration/CardsExplorer"
+import { InfiniteScrollTrigger } from "components/Utils/InfiniteScrollTrigger"
+import { CardsContainer } from "components/Card/CardsContainer"
+import { LargeGentkCard } from "components/Card/LargeGentkCard"
+import { CardsLoading } from "components/Card/CardsLoading"
+import { useInfiniteScroll } from "hooks/useInfiniteScroll"
+import { useQuery } from "@apollo/client"
+import { Qu_genTokenIterations } from "queries/generative-token"
+import { Objkt } from "types/entities/Objkt"
+import { GentkList } from "components/GenerativeToken/GentkList"
 
+const ITEMS_PER_PAGE = 20
 interface GenerativeRedeemProps {
   token: GenerativeToken
   redeemableDetails: RedeemableDetails[]
@@ -20,6 +31,35 @@ const _GenerativeRedeem = ({
   token,
   redeemableDetails,
 }: GenerativeRedeemProps) => {
+  const { data, loading, fetchMore } = useQuery(Qu_genTokenIterations, {
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      id: token.id,
+      skip: 0,
+      take: ITEMS_PER_PAGE,
+      filters: {
+        redeemable_eq: true,
+      },
+    },
+    fetchPolicy: "cache-and-network",
+    nextFetchPolicy: "cache-and-network",
+  })
+
+  const tokens: Objkt[] | null = data?.generativeToken?.objkts
+
+  const { topMarkerRef, onEndReached } = useInfiniteScroll({
+    loading,
+    itemLength: tokens?.length || 0,
+    onFetchMore: () => {
+      fetchMore?.({
+        variables: {
+          skip: tokens?.length || 0,
+          take: ITEMS_PER_PAGE,
+        },
+      })
+    },
+  })
+
   const renderBackToProject = (
     <div className={cs(layout.flex_column_left)}>
       <Link href={getGenerativeTokenUrl(token)}>
@@ -49,6 +89,26 @@ const _GenerativeRedeem = ({
           />
         ))}
       </div>
+      <Spacing size="x-large" />
+      <CardsExplorer cardSizeScope="generative-iteration">
+        {({ refCardsContainer, cardSize }) => (
+          <>
+            <div ref={topMarkerRef} />
+
+            <div style={{ width: "100%" }}>
+              <GentkList
+                tokens={tokens}
+                loading={loading}
+                cardSize={cardSize}
+                itemsPerPage={ITEMS_PER_PAGE}
+                canTrigger={!!data && !loading}
+                onEndReached={onEndReached}
+                refCardsContainer={refCardsContainer}
+              />
+            </div>
+          </>
+        )}
+      </CardsExplorer>
       <Spacing size="x-large" />
       {renderBackToProject}
     </div>
