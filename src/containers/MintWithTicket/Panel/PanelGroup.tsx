c6ed7c1234518a47363efb824fe062add04943e9
louis <louisholley@me.com>
2023-06-09 09:59:01 +0100

    make PanelGroup optionally collapsible

--- src/containers/MintWithTicket/Panel/PanelGroup.tsx
+++ src/containers/MintWithTicket/Panel/PanelGroup.tsx
@@ -2,57 +2,66 @@ import style from "./PanelGroup.module.scss"
 import cs from "classnames"
 import { PropsWithChildren, ReactChild } from "react"
 import { useAriaTooltip } from "hooks/useAriaTooltip"
+import { Collapsible } from "components/Collapsible/Collapsible"
 
 export type PanelGroupProps = PropsWithChildren<{
   title: string
   description?: string
   descriptionClassName?: string
   headerComp?: ReactChild
+  collapsible?: boolean
 }>
 export function PanelGroup({
   title,
   headerComp,
   description,
   descriptionClassName,
+  collapsible = false,
   children,
 }: PanelGroupProps) {
   const { showTooltip, handleLeave, handleEnter, hoverElement } =
     useAriaTooltip()
   return (
     <div className={cs(style.root)}>
-      <div className={cs(style.header)}>
-        <div className={cs(style.title_wrapper)}>
-          <h2 className={cs(style.title)}>{title}</h2>
-          {description && (
-            <i
-              ref={hoverElement}
-              className={cs(
-                "fa-sharp fa-solid fa-circle-info",
-                style.info_icon
+      <Collapsible
+        disabled={!collapsible}
+        header={
+          <div className={cs(style.header)}>
+            <div className={cs(style.title_wrapper)}>
+              <h2 className={cs(style.title)}>{title}</h2>
+              {description && (
+                <i
+                  ref={hoverElement}
+                  className={cs(
+                    "fa-sharp fa-solid fa-circle-info",
+                    style.info_icon
+                  )}
+                  onMouseEnter={handleEnter}
+                  onMouseLeave={handleLeave}
+                  onBlur={handleLeave}
+                  onFocus={handleEnter}
+                  tabIndex={0}
+                />
               )}
-              onMouseEnter={handleEnter}
-              onMouseLeave={handleLeave}
-              onBlur={handleLeave}
-              onFocus={handleEnter}
-              tabIndex={0}
-            />
-          )}
-          {description && showTooltip && (
-            <span
-              role="tooltip"
-              aria-live="polite"
-              aria-hidden={!showTooltip}
-              className={style.tooltip}
-            >
-              {description}
-            </span>
-          )}
-        </div>
-        {headerComp && (
-          <div className={cs(style.header_child)}>{headerComp}</div>
-        )}
-      </div>
-      <div className={cs(style.content)}>{children}</div>
+              {description && showTooltip && (
+                <span
+                  role="tooltip"
+                  aria-live="polite"
+                  aria-hidden={!showTooltip}
+                  className={style.tooltip}
+                >
+                  {description}
+                </span>
+              )}
+            </div>
+            {headerComp && (
+              <div className={cs(style.header_child)}>{headerComp}</div>
+            )}
+          </div>
+        }
+      >
+        <div className={cs(style.content)}>{children}</div>
+      </Collapsible>
     </div>
   )
 }
