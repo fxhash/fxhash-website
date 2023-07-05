958a3bbeefe0f573fe2f22449c243ab3fa40adcc
louis <louisholley@me.com>
2023-07-05 12:08:55 +0100

    retry fetch random seed up to 5 times

--- src/hooks/useFetchRandomSeed.ts
+++ src/hooks/useFetchRandomSeed.ts
@@ -1,14 +1,31 @@
 import { useQuery } from "@apollo/client"
 import { Qu_reveal } from "queries/reveal"
+import { useEffect, useState } from "react"
+
+const MAX_RETRIES = 5
 
 export function useFetchRandomSeed(opHash: string | null) {
-  const { data, loading, error } = useQuery(Qu_reveal, {
+  const [retries, setRetries] = useState(0)
+  const [error, setError] = useState<Error | null>(null)
+
+  const { data, loading, refetch } = useQuery(Qu_reveal, {
     variables: {
       hash: opHash,
     },
     fetchPolicy: "no-cache",
     skip: !opHash,
+    onError: () => {
+      if (retries < MAX_RETRIES) {
+        setRetries(retries + 1)
+      } else {
+        setError(new Error("Maximum retry attempts reached"))
+      }
+    },
   })
+
+  useEffect(() => {
+    if (data === null && retries < 3) refetch()
+  }, [data, retries])
 
   return {
     loading,
