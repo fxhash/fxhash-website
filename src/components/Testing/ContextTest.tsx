edbceccffa43870885f5b8f901374150a148749a
maerzhase <markus@lili16.de>
2023-07-05 19:39:15 +0200

    add option to display contexttest as buttons

--- src/components/Testing/ContextTest.tsx
+++ src/components/Testing/ContextTest.tsx
@@ -1,16 +1,18 @@
 import styles from "./ContextTest.module.scss"
 import { TExecutionContext } from "hooks/useRuntime"
 import { Select } from "components/Input/Select"
+import { Button } from "components/Button"
 
 interface Props {
   value: TExecutionContext | null | undefined
   onChange: (value: TExecutionContext) => void
+  asButtons?: boolean
 }
 
-const contexts = ["minting", "standalone", "capture"]
+const contexts: TExecutionContext[] = ["minting", "standalone", "capture"]
 
 export function ContextTest(props: Props) {
-  const { onChange, value } = props
+  const { onChange, value, asButtons = false } = props
 
   const handleChange = (context: TExecutionContext) => {
     onChange(context)
@@ -19,15 +21,31 @@ export function ContextTest(props: Props) {
   return (
     <div className={styles.contexttest}>
       <small>Context</small>
-      <Select
-        className={styles.select}
-        value={value}
-        onChange={handleChange}
-        options={contexts.map((c) => ({
-          value: c,
-          label: c,
-        }))}
-      />
+      {asButtons ? (
+        <div className={styles.buttons}>
+          {contexts.map((context) => (
+            <Button
+              className={styles.button}
+              size="small"
+              key={context}
+              onClick={() => handleChange(context)}
+              color={value === context ? "primary" : "black"}
+            >
+              {context}
+            </Button>
+          ))}
+        </div>
+      ) : (
+        <Select
+          className={styles.select}
+          value={value}
+          onChange={handleChange}
+          options={contexts.map((c) => ({
+            value: c,
+            label: c,
+          }))}
+        />
+      )}
     </div>
   )
 }
